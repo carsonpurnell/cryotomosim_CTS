138c9c009ddd3f6b44626321897211362a0a1b2d
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-08-26 13:41:30 -0400

    Update helper_arrayinsert.m

--- helper_arrayinsert.m
+++ helper_arrayinsert.m
@@ -19,17 +19,28 @@ dx=max(1,coord(1)):min(d1,coord(1)+s1-1);
 dy=max(1,coord(2)):min(d2,coord(2)+s2-1);
 dz=max(1,coord(3)):min(d3,coord(3)+s3-1);
 
+
+%linear index is slower than direct indexing, probably because it needs to reshape internally
+%s = [d1,d2,d3]; %size of array to index
+%index = dx.' + s(1)*(dy-1) + s(1)*s(2)*reshape(dz-1,1,1,numel(dz));
+%if ~dest(index)==dest(dx,dy,dz), fprintf('xx'); end %test identity
+
+
 %compute indexes for the relevant region of the source
 sx=max(-coord(1)+2,1):min(d1-coord(1)+1,s1);
 sy=max(-coord(2)+2,1):min(d2-coord(2)+1,s2);
 sz=max(-coord(3)+2,1):min(d3-coord(3)+1,s3);
 
+
 switch method
     case 'sum'
-        dest(dx,dy,dz) = source(sx,sy,sz) + dest(dx,dy,dz);
+        tmp = source(sx,sy,sz) + dest(dx,dy,dz);
+        dest(dx,dy,dz) = tmp;
+        %dest2(dind) = tmp;
     case 'nonoverlap' %first test if there would be overlap to save time
         %dl = logical(dest(dx,dy,dz)); sl = logical(source(sx,sy,sz)); %faster but too inclusive
-        dbin = imbinarize(rescale(dest(dx,dy,dz))); sbin = imbinarize(rescale(source(sx,sy,sz)));
+        dbin = imbinarize(rescale(dest(dx,dy,dz))); 
+        sbin = imbinarize(rescale(source(sx,sy,sz)));
         
         olog = dbin+sbin; olog = max(olog(:)); %fastest method to find potential overlaps
         if olog>1 %if overlap, record and output original
