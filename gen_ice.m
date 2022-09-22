1a0f78ce93f80e8e3dfdd4336c79f6b43ba88d6b
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-09-22 13:06:20 -0400

    Update gen_ice.m

--- gen_ice.m
+++ gen_ice.m
@@ -1,7 +1,6 @@
 function [iced, ice] = gen_ice(vol,pix)
-
 w = 8+2; %atomic number total in water molecule, until i can get electron opacity numbers
-%amorphous ice density .94g/cm^3, but unit vol of liquid h20 molecule is 29.7a^3, estimate 20-30
+%amorphous ice density ~.94g/cm^3, but unit vol of liquid h20 molecule is 29.7a^3, estimate 20-30
 %unit vol of water is 29.7a^3, dramatically larger than atoms, diameter ~2.75A.
 density = 0.90/(1e8)^3/18*6.022e23; %convert from cm3 to a3, then g to molecules via mol/dalton
 
@@ -15,8 +14,7 @@ pts = rand(3,mol).*size(vol)'; ix = round(pts+0.5); %fast pregeneration of all p
 for i=1:mol
     co = ix(:,i); x = co(1); y = co(2); z = co(3); %slightly faster column indexing
     ice(x,y,z) = ice(x,y,z)+w*1;
-   % if rem(i,round(mol/10))==0, fprintf(',%g',i/mol); end
-end %fprintf('\n')
+end
 
 iced = max(ice,vol);
 end
