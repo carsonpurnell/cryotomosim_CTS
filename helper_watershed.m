1844f8869039224228893860b0edae65b028c1c8
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-10-24 11:29:36 -0400

    Create helper_watershed.m

---
+++ helper_watershed.m
@@ -0,0 +1,17 @@
+%function labelmask = helper_watershed(vol)
+%
+%
+vol = trim;
+cl = imclose(vol,strel('sphere',2)); %close small holes to avoid chopping most particles up
+cl = imclose(cl,strel('sphere',2));
+%is there a better way to safely dilate, by smaller than whole pixels?
+
+d = -bwdist(~cl); %calculate distances for watershed
+mask = imextendedmin(d,1); %generate extended minima mask from distance map (not sure why 2)
+d2 = imimposemin(d,mask); %merge local minima to make segmentations less chopped
+w = watershed(d2); w2 = w;
+w(~cl) = 0; %watershed the local minima and mask out non-particles
+
+labelmask = w;
+
+%end
