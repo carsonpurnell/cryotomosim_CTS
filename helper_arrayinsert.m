f73fa215b44b23d81a5889b62b8414823484e765
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-01 13:47:14 -0400

    Update helper_arrayinsert.m

--- helper_arrayinsert.m
+++ helper_arrayinsert.m
@@ -52,9 +52,9 @@ switch method
         %tmp2 = source(sxi:sxf,syi:syf,szi:szf) + dest(dxi:dxf,dyi:dyf,dzi:dzf); dest(dxi:dxf,dyi:dyf,dzi:dzf) = tmp2;
         %71
         
-        dest(dx,dy,dz) = source(sx,sy,sz) + dest(dx,dy,dz); %41
+        %dest(dx,dy,dz) = source(sx,sy,sz) + dest(dx,dy,dz); %75
         
-        %tmp1 = source(sx,sy,sz) + dest(dx,dy,dz); dest(dx,dy,dz) = tmp1; %42
+        tmp1 = source(sx,sy,sz) + dest(dx,dy,dz); dest(dx,dy,dz) = tmp1; %42
     case 'nonoverlap' %first test if there would be overlap to save time
         %dl = logical(dest(dx,dy,dz)); sl = logical(source(sx,sy,sz)); %faster but too inclusive
         dbin = imbinarize(rescale(dest(dx,dy,dz))); sbin = imbinarize(rescale(source(sx,sy,sz)));
