91dfa992d58677355abc4b10aac71344e56d2e9e
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-10 14:29:51 -0500

    Update helper_watershed.m

--- helper_watershed.m
+++ helper_watershed.m
@@ -2,15 +2,18 @@ function [labelmask,fields] = helper_watershed(vol)
 
 %vol = trim;
 bin = imbinarize(rescale(vol));
-cl = imclose(bin,strel('sphere',2)); %close small holes to avoid chopping most particles up
-cl = abs((bwdist(cl)>1)-1);
+cl = imclose(bin,strel('sphere',1)); %close small holes to avoid chopping most particles up
+cl = bin;
+%cl = abs((bwdist(cl)>1)-1);
 %cl = imopen(cl,strel('sphere',2));
 %cl = imclose(cl,strel('sphere',2));
 %is there a better way to safely dilate, by smaller than whole pixels?
 
+%can i use an additional input as the mask/fieldshed?
+
 d = -bwdist(~cl); %calculate distances for watershed
-%mask = imextendedmin(d,2); %generate extended minima mask from distance map (not sure why 2)
-mask = bwareaopen(imerode(cl,strel('sphere',2)),4); %might work better?
+mask = imextendedmin(d,2); %generate extended minima mask from distance map (not sure why 2)
+%mask = bwareaopen(imerode(cl,strel('sphere',2)),4); %might work better?
 d2 = imimposemin(d,mask); %merge local minima to make segmentations less chopped
 w = watershed(d2,26); fields = single(w);
 w(~bin) = 0; %watershed the local minima and mask out non-particles
