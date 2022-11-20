dc6fd34fd66d4a8ca0c0c79c9acb624bb302de33
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-20 15:07:38 -0500

    Update helper_electrondetect.m

--- helper_electrondetect.m
+++ helper_electrondetect.m
@@ -14,7 +14,7 @@ end
 if param.dose<=0, detect=tilt; return; end %if dose 0, skip detection and return perfect detection/original
 tiltangs = param.tilt; %unfortunately similar name to tilt 
 
-DQE = .84*4; % gatan camera lists 84% maximum detection, so that'll work for now
+DQE = .84*1; % gatan camera lists 84% maximum detection, so that'll work for now
 %4 is arbitrary scalar to make contrast look 'normal' with the CTF modulation
 %DQE should not be angle dependent, but maybe easier to  implement if merged with CTF?
 
