7a615a4761b351f99208ec49d048e4ce291b313e
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-28 12:41:22 -0500

    feat ctsutil for cross-function utilities

--- ctsutil.m
+++ ctsutil.m
@@ -1,8 +1,15 @@
 function out = ctsutil(op,in,opt)
+%out = ctsutil(op,in,opt)
+%'trim' in=vol
+%'pad'  in=vol opt=padding, all dims
+%'bin'  in=vol opt=binning
 switch op
     case 'trim'
         out = in(any(in ~= 0,[2 3]),any(in ~= 0,[1 3]),any(in ~= 0,[1 2]));
     case 'pad'
         out = padarray(in,[opt,opt,opt]);
-
+    case 'bin'
+        opt = max(2,round(opt)); %avoid bad bin value errors
+        
+        
 end
