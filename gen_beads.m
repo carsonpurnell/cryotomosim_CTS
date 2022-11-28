f8e5d038b19fa1d840196d19f89356a7ce4643d3
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-28 15:08:11 -0500

    Update gen_beads.m

--- gen_beads.m
+++ gen_beads.m
@@ -6,37 +6,33 @@ arguments
     pix (1,1) {mustBePositive}
     radius (1,:) = 50 %can use multiple as [40 60 120]
 end
-%calculate gold atomic density in atom/pix from 19g/cm^3
-%atomdensity = d/(cm-A conv)*avagadro/mass
-density = (19/1e8^3)*6e23/197; %calculate atoms/A^3, ~.06
-density = density*pix^3; %average atoms per pixel
-Au = 79; %atomic number of gold
+if isempty(radius), radius = 50; end %backstop for empty but extant radius
+
+density = (19*6e23/197)*(pix/1e8)^3; %d = (d*mol/mass)*(pixel/m-a conv)^3 average atom/pix for gold 19g/cm3
+Au = 79; %atomic number of gold, until atom opacities figured out
 %probably don't need to do density/point tradeoff with mass/points being static across pixel sizes
 
 %are real beads uniform density? hollow core might be from edge effect of uniform opacity
 %lower border density might help close the hole
 
-beadstrc(numel(radius)).type = 'single';
+beadstrc.type = 'single';
 for j=1:numel(radius)
-    d = round(radius(j)*2/pix)+2; %rad in A, diam in pixels
-    cen = radius(j)/pix+1;
+    d = round(radius(j)*2/pix)+2; %generate empty volume for bead points
+    cen = radius(j)/pix+0.5; %center for distance to be calculated from
     
-    gold = zeros(d,d,d);
-    atoms = 1*round(density*numel(gold)*1); % double points at half mass for smoothness
-    %probably don't need this now? can make it worse on purpose?
+    gold = zeros(d,d,d); %generate temporary array to fill with bead
+    atoms = 1*round(density*numel(gold)*1); % calculate total number of points for the volume
     
     pts = rand(3,atoms).*size(gold)'; %pregenerate points
-    atomdist = sqrt(sum( (pts-cen).^2, 1)); %marginally faster
+    atomdist = sqrt(sum( (pts-cen).^2, 1)); %calculate distance of each point for pruning
     pts = round(pts(:,atomdist<radius(j)/pix)+0.5); %remove atom points that would be outside sphere
     
     for i=1:size(pts,2)
         x = pts(1,i); y = pts(2,i);  z = pts(3,i);
-        gold(x,y,z) = gold(x,y,z) + Au/1; %reducing gold signal to prevent protein overlap, needs work
+        gold(x,y,z) = gold(x,y,z) + Au/1; %add gold intensity for each generated atom in the bead
     end
-    gold = ctsutil('trim',gold);
-    
-    beadstrc(j).vol{1} = gold;
-    beadstrc(j).id = append('bead__',string(radius(j)));
+    beadstrc.vol{j} = ctsutil('trim',gold); %write vol and id to the struct to pass to cts_model
+    beadstrc.id{j} = append('bead__',string(radius(j)));
 end
 
 end
