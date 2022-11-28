80f59aaeb7be30800738cb6d264f4998b73fa8c8
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-28 12:29:57 -0500

    Update ctsutil.m

--- ctsutil.m
+++ ctsutil.m
@@ -1,6 +1,8 @@
 function out = ctsutil(op,in,opt)
-
-
-
+switch op
+    case 'trim'
+        out = in(any(in ~= 0,[2 3]),any(in ~= 0,[1 3]),any(in ~= 0,[1 2]));
+    case 'pad'
+        out = padarray(in,[opt,opt,opt]);
 
 end
