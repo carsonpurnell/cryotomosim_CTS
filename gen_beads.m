b48dbd891094e136e882bc85432bbe80a625a556
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-11-28 15:18:07 -0500

    Update gen_beads.m

--- gen_beads.m
+++ gen_beads.m
@@ -1,7 +1,8 @@
 function [beadstrc] = gen_beads(pix,radius)
-%no help yet you're on your own
-%but use angstroms
-%radius default 50, outputs the same number of different beads as input radii
+%[beadstrc] = gen_beads(pix,radius)
+%generates a CTS-format particleset of gold beads based on computed atomic density
+%pix = pixel resolution of the beads in angstroms
+%radius = vector of radii of different beads required. default 50
 arguments
     pix (1,1) {mustBePositive}
     radius (1,:) = 50 %can use multiple as [40 60 120]
@@ -11,7 +12,6 @@ if isempty(radius), radius = 50; end %backstop for empty but extant radius
 density = (19*6e23/197)*(pix/1e8)^3; %d = (d*mol/mass)*(pixel/m-a conv)^3 average atom/pix for gold 19g/cm3
 Au = 79; %atomic number of gold, until atom opacities figured out
 %probably don't need to do density/point tradeoff with mass/points being static across pixel sizes
-
 %are real beads uniform density? hollow core might be from edge effect of uniform opacity
 %lower border density might help close the hole
 
@@ -19,14 +19,10 @@ beadstrc.type = 'single';
 for j=1:numel(radius)
     d = round(radius(j)*2/pix)+2; %generate empty volume for bead points
     cen = radius(j)/pix+0.5; %center for distance to be calculated from
-    
     gold = zeros(d,d,d); %generate temporary array to fill with bead
-    atoms = 1*round(density*numel(gold)*1); % calculate total number of points for the volume
-    
-    pts = rand(3,atoms).*size(gold)'; %pregenerate points
+    pts = rand(3,round(density*numel(gold))).*size(gold)'; %pregenerate points from computed atomic density
     atomdist = sqrt(sum( (pts-cen).^2, 1)); %calculate distance of each point for pruning
     pts = round(pts(:,atomdist<radius(j)/pix)+0.5); %remove atom points that would be outside sphere
-    
     for i=1:size(pts,2)
         x = pts(1,i); y = pts(2,i);  z = pts(3,i);
         gold(x,y,z) = gold(x,y,z) + Au/1; %add gold intensity for each generated atom in the bead
