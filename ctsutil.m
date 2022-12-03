2002bd59e4e1b428cee8dec75b51a10df93a86ac
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-12-03 12:16:54 -0500

    Update ctsutil.m

--- ctsutil.m
+++ ctsutil.m
@@ -5,7 +5,8 @@ function out = ctsutil(op,in,opt)
 %'bin'  in=vol opt=binning
 switch op
     case 'trim'
-        out = in(any(in ~= 0,[2 3]),any(in ~= 0,[1 3]),any(in ~= 0,[1 2]));
+        %out = in(any(in ~= 0,[2 3]),any(in ~= 0,[1 3]),any(in ~= 0,[1 2]));
+        out = in(any(in,[2 3]),any(in,[1 3]),any(in,[1 2])); %slightly faster, avoid repeat logicals
     case 'pad'
         out = padarray(in,[opt,opt,opt]);
         
