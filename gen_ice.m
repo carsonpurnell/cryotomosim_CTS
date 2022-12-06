15e4249d789060a98797116ca1b9e856f929cc69
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-12-06 11:35:19 -0500

    Update gen_ice.m

--- gen_ice.m
+++ gen_ice.m
@@ -7,9 +7,10 @@ denspix = (.94/18)*6e23*(pix/1e8)^3; %d = (d/mass)*mol*(pixel/m-a conv)^3 averag
 atomfrac = exp(-pix/3); %fraction as points rather than flat background
 %does compressing it into fewer points of higher density work without screwing the noise?
 densfrac = 20/(20+pix)*1; %scalar to distribute extra intensity to particles to reduce number required
-w = (8+2)/densfrac; %scaled intensity for water psuedo-molecules (atomic number until i find e- opacties)
+w = 8+2;
 mol = round(denspix*numel(vol)*atomfrac*densfrac); % atomfrac% of ice mass randomly distributed as molecules
 ice = round(vol*0+denspix*(1-atomfrac)*w); % 1-atomfrac% of ice mass as flat background for speed
+w = w/densfrac; %scaled intensity for water psuedo-molecules (atomic number until i find e- opacties)
 
 pts = rand(3,mol).*size(vol)'; ix = round(pts+0.5); %fast pregeneration of all points
 for i=1:mol
@@ -21,10 +22,11 @@ end
 % take min of smoothed vol+ice for a halo?
 
 %orig straight up floor by ice globally
-iced = max(ice,vol);
+iced = max(ice,vol); %for some reason the ice is much too dense with test TMD placement output
 %
 %alternate maybe easier binarization method
-solv = imbinarize(rescale(vol)); map = imgaussfilt3(single(~solv),4);%,'FilterSize',3);
+solv = imbinarize(rescale(vol)); 
+map = imgaussfilt3(single(~solv),3);%,'FilterSize',3);
 %for camk at 6A, ~2 sig, 3 too high and 1 too little SNR. probably need more radiation effect
 %for actin at 13A, ~2 sig, 1 SNR is crap - =<2 needs more noise (maybe also more rad damage via noise)
 
