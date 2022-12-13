079b1f9c83d5afd5c3e2ea61b2bca0587fa19731
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-12-13 10:37:07 -0500

    Update helper_particleatlas.m

--- helper_particleatlas.m
+++ helper_particleatlas.m
@@ -15,8 +15,9 @@ arguments
 end
 %move other important particles to splitmodel if they exist, at the end for ease
 if isfield(cts.model,'beads'), cts.splitmodel.beads = cts.model.beads; end
-if isfield(cts.model,'mem'), cts.splitmodel.mem = cts.model.mem; end
+if isfield(cts.model,'mem'), cts.splitmodel.AAAAAmem = cts.model.mem; end %makes membrane first placement
 if isfield(cts.model,'grid'), cts.splitmodel.grid = cts.model.grid; end
+cts.splitmodel = orderfields(cts.splitmodel);
 
 roinames = fieldnames(cts.splitmodel); %retrieve component names
 indvol = cell(1,numel(roinames));
@@ -25,9 +26,12 @@ atlas = zeros(size(cts.splitmodel.(roinames{1})));
 for i=1:numel(roinames)
     indvol{i} = cts.splitmodel.(roinames{i}); %add model to stack
     bin = imbinarize(rescale(indvol{i})); %binarize model to add to atlas
-    atlas = atlas+bin*i; %generate label image atlas based on model order for label intensity
-    overlap = atlas>i; %make a mask of overlap regions 
-    atlas = imfill(atlas-overlap); %remove overlap regions and fill with neighbor values
+    atlas(bin>0) = i;
+    
+    %atlas = atlas+bin*i; %generate label image atlas based on model order for label intensity
+    %overlap = atlas>i; %make a mask of overlap regions 
+    %atlas = imfill(atlas-overlap); %remove overlap regions and fill with neighbor values
+    
     filename = append('ind',string(i),'_',roinames{i});
     if individual==1
         WriteMRC(bin,cts.pix,append(filename,'.mrc'))
