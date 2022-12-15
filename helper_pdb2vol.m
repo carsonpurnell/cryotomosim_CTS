7f7c9c5e005f15a95e51b1abab2584656eb7d5a9
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2022-12-15 16:42:39 -0500

    Update helper_pdb2vol.m

--- helper_pdb2vol.m
+++ helper_pdb2vol.m
@@ -55,7 +55,7 @@ end
 function [data] = internal_pdbparse(pdb)
 fid = fileread(pdb); 
 text = textscan(fid,'%s','delimiter','\n'); %slightly faster to not parse remarks at all
-%text = textscan(fid,'%s','delimiter','\n','CommentStyle',{'REMARK'}); %import each line individually
+text = textscan(fid,'%s','delimiter','\n');%,'CommentStyle',{'REMARK'}); %import lines, ignoring comments slow
 text = text{1}; %fix being inside a 1x1 cell array
 
 %pdb appear to have no means of storing model names
