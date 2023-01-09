f076f4233d41991e1fe409ac58e71ff4b3bde7e1
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-01-09 12:43:51 -0500

    Update param_model.m

--- param_model.m
+++ param_model.m
@@ -4,6 +4,10 @@ function param = param_model(in)
 arguments
     in
 end
+%make preliminary list of all the parameters
+%figure out the bounds of each and different use cases
+
+%what are non-params? need suffix opt separately, that's not a model parameter.
 
 param = in;
 
