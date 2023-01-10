f6fc682b5d7b0d985e670a7c7d0e23029a1c7bae
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-01-10 10:51:04 -0500

    Update param_model.m

--- param_model.m
+++ param_model.m
@@ -5,21 +5,24 @@ arguments
     %maybe GUI input dlg
     %easy way to navigate to stored params? modifiable defaults?
     
+    %required for function params
     param.vol (:,:,:) = zeros(400,400,50) %must be input?
     param.pix (1,1) %must be input?
     
+    %input particles params
     param.targets = 'gui' %soft required?
     param.distractors = 'none'
     
+    %model run and limitations params
     param.density = 0.4 %if moving to target loop, this needs to be able to be a vector
     param.iters = [] %auto calculate if not given, would also need to be vectorable
     param.constraint string {mustBeMember(param.constraint,{'none','box','tube','sides'})} = 'sides'
     %change constraint to a more flexible x/y/z for different oriented tube/walls?
     
+    %objects and contents params
     param.grid = [15 2000]
     param.mem = 0 %need subcomponents
     param.bead = 0
-    
     param.ice = 1 %need more control. also could do with surface ice contamination, and more roughness
     
 end
