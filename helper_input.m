0c1b0afa5f94d4d385365d7ca5fa9349ac70bcf2
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-01-11 09:51:08 -0500

    Update helper_input.m

--- helper_input.m
+++ helper_input.m
@@ -61,18 +61,19 @@ for i=1:numel(list)
         [tmp.vol,sumvol,names] = helper_pdb2vol(list{i},pixelsize,trim,centering,sv); 
         %read pdb and construct as volume at pixel size
         %pdb names read in as 'NA', cif are in column cell array of strings
-        fprintf('generating at %g A ',pixelsize)
+        %fprintf('generating at %g A ',pixelsize)
     elseif iscellstr(list(i)) && strcmp(ext,'.mrc')
         fprintf('loading %s  ',filename)
         [tmp, head] = ReadMRC(list{i});
-        fprintf('resizing from %g to %g pixel size',head.pixA,pixelsize)
+        %fprintf('resizing from %g to %g pixel size',head.pixA,pixelsize)
         tmp.vol = imresize3(tmp,head.pixA/pixelsize);
         names = {'NA'};
     elseif iscellstr(list(i)) %#ok<*ISCLSTR>
-        error('Error: item %i in the input list is a string, but not a valid file type',i)
+        error('Error: item %i (%s) in the input list is a string, but not a valid file type',i,list{i})
     end
+    fprintf('generating at %g A ',pixelsize)
     
-    % parse names block, might go after parsing files
+    % parse names block, might go after loading files
     id = strrep(id,'-','_'); %change dashes to underscore, field names can't have dashes
     for j=1:numel(id) %loop through ID parts to make them functional for field names
         id{j} = string(id{j}); %convert to string for consistency with other functions
