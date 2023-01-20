36b3ce2819aec2cd8c20598e7f050bb5cbd563c7
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-01-20 09:38:02 -0500

    Update helper_constraints.m

--- helper_constraints.m
+++ helper_constraints.m
@@ -2,15 +2,12 @@ function vol = helper_constraints(vol,edges)
 %vol = helper_constraints(vol,edges)
 %outputs an empty copy of vol, with 1s written on certain borders define by edgges input
 %1x3 char array as '&&&' in yxz(matlab) and xyz(IMOD) oreientation, + is top, - bottom, space none, & both
-
-%make the [1,end] vector into the needed version for each entry first then use them?
-%and empty [] vector writes nothing, usefully
 vec = cell(1,numel(edges));
 for i=1:numel(edges)
     for j=1:3
-        vec{j} = 1:size(vol,j);
+        vec{j} = 1:size(vol,j); %initialize vector triad
     end
-    switch edges(i)
+    switch edges(i) %write the subject vector to list the appropriate border indices
         case "+"
             vec{i} = size(vol,i);
         case "-"
@@ -18,9 +15,8 @@ for i=1:numel(edges)
         case "&"
             vec{i} = [1,size(vol,i)];
         case " "
-            vec{i} = [];
+            vec{i} = []; %empty vector writes nothing when used so if check not needed
     end
     vol(vec{1},vec{2},vec{3}) = 1;
 end
-
 end
