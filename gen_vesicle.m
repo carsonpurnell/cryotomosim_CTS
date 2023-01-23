402f2b3b9658e598a713dd9727f14bd4862e350c
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-01-22 19:13:54 -0500

    Update gen_vesicle.m

--- gen_vesicle.m
+++ gen_vesicle.m
@@ -31,6 +31,8 @@ count.s = 0; count.f = 0;
 memvol = vol*0;
 vescen = []; vesvol = {}; %store centers and location vols for each placed vesicle for TMD operations
 for i=1:num
+    
+    %{
     radi = (rand*700+150)/pix; %randomly generate inner radius of vesicle (need better range)
     rado = radi+(14+randi(14))/pix; %get outer radius from inner, should be constant something (7-9nm-ish?)
     %reduced outer radius distance for pearson, skew makes it wider
@@ -72,6 +74,8 @@ for i=1:num
         tmp(x(j),y(j),z(j)) = tmp(x(j),y(j),z(j)) + lipid;
     end
     tmp = ctsutil('trim',tmp);
+    %}
+    tmp = vesgen_sphere(pix);
     ves{i} = tmp; %#ok<AGROW> %store trimmed vesicle into output cell array
     
     for q=1:tries %try to place each vesicle N times, allows for duplicates
@@ -107,6 +111,50 @@ pts = [x,y,z];
 
 %
 
+end
 
+function ves = vesgen_sphere(pix)
+
+radi = (rand*700+150)/pix; %randomly generate inner radius of vesicle (need better range)
+rado = radi+(14+randi(14))/pix; %get outer radius from inner, should be constant something (7-9nm-ish?)
+%reduced outer radius distance for pearson, skew makes it wider
+offset = round(rado+20); %centroid offset to prevent negative values
+%still not sure how to do the radius and what the radial density curve should look like
+
+w = (rado-radi)/1.5; %deviation of the membrane distribution
+sf = [(rado^2)/(radi^2),(radi^2)/(rado^2)]/2; %factor to correct for excess inner density
+
+%fill space between radii with tons of points
+%ptnum = round(radi*5*(pix^3)*pi^2); %need to actually calculate volume of shell
+shellvol = 4/8*pi*(rado^3-radi^3); %volume of shell in pixels
+ptnum = round( 0.2*shellvol*pix^3 )*2; %convert to angstroms, scale to some arbitrary working density
+frac = [ptnum,ptnum*sf(2),ptnum*sf(1)]; %get fractions of the total to distribute between inner and outer
+rti = round(ptnum*sf(2)); rto = ptnum-rti; %partition density between inner and outer radii
+
+%ptrad = rand(ptnum,1)*(rado-radi)+radi; %uniform - flat monolayer
+switch 1
+    case 1 %mirrored pearson - relatively hard inner and outer edges
+        ptrad = [pearsrnd(radi,w,0.7,3,rti,1);pearsrnd(rado,w,-0.7,3,rto,1)];
+    case 2 %mirrored gamma - a bit narrower, more edge smoothing
+        ptrad = radi+[betarnd(3.0,6,rti,1);betarnd(6,3.0,rto,1)]*(rado-radi)*3.5;
+end
+%pearson is very slow, calls beta to call gamma which takes most of the time
+%need to reformulate the math so that density is hard-bound between ri/ro in angstroms
+%figure(); histogram(ptrad);
+
+ptaz = rand(ptnum,1)*pi*2; %random circular azimuth angles
+%ptel = rand(1,ptnum)*pi*2; %causes asymmetry, polar density accumulation
+ptel = asin(2*rand(ptnum,1)-1); %random elevation angles, corrected for polar density accumulation
+
+[x,y,z] = sph2cart(ptaz,ptel,ptrad); %convert spherical coords to cartesian coords
+
+%generate empty array and round points to positive coords
+tmp = zeros(offset*2,offset*2,offset*2);
+x = round(x+offset); y = round(y+offset); z = round(z+offset);
+lipid = 5.5; %need to find the typical density of lipid membrane
+for j=1:numel(x) %loop through and add points as density to the shell
+    tmp(x(j),y(j),z(j)) = tmp(x(j),y(j),z(j)) + lipid;
+end
+ves = ctsutil('trim',tmp);
 
 end
