e169f240ca3de1b7cb596b8a8e7ae9c3a8af20dc
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-10 15:30:28 -0500

    Update atomicmodeling_scripttesting.m

--- atomicmodeling_scripttesting.m
+++ atomicmodeling_scripttesting.m
@@ -281,7 +281,7 @@ end
 
 
 %% function for vol, atlas, and split generation
-[vol,solv,splitvol] = helper_atoms2vol(pix,split(2:end),boxsize);
+[vol,solv,atlas,splitvol] = helper_atoms2vol(pix,split(2:end),boxsize);
 
 
 %% functionalized volume projection
