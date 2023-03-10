7e44cdfd654734c8d5cdb6a1b5c04dbdca1d0d05
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-09 23:05:45 -0500

    Update atomicmodeling_scripttesting.m

--- atomicmodeling_scripttesting.m
+++ atomicmodeling_scripttesting.m
@@ -289,10 +289,12 @@ sliceViewer(em);
 %% solvation op2: flat density+variance window
 %subtract volume from each voxel, estimate waters per voxel from remaining volume with random portion
 %works well, need better values and improved randomization. should probably be built into helper_pt2vol
+%now is built in. kind of.
 tic
 allatoms = vertcat(split{2:end,1});
-solvvol = ifcn_solv(pix,allatoms(:,1:3),boxsize); %similar to helper_pt2vol
-sliceViewer(solvvol+em);
+%solvvol = ifcn_solv(pix,allatoms(:,1:3),boxsize); %similar to helper_pt2vol
+[vol,solv] = helper_atoms2vol_i(pix,allatoms,boxsize);
+sliceViewer(solv+vol);
 toc
 
 %% solvation testing
@@ -337,6 +339,33 @@ sliceViewer(em+watervol);
 %}
 
 %% internal functions
+
+function [vol,solv] = helper_atoms2vol_i(pix,pts,sz,offset)
+if nargin<4, offset=[0,0,0]; end
+if nargin<3, sz = max(pts,[],1)+pix; end
+%if size(pts,2)<4, pts(:,end+1)=1; end %intensity==1 if not given by 4th column
+%need rough estimate of average volume for organic atoms
+%very approximately 1.8a radii
+%eventually might do individual vdw radii individually
+avol = 4/3*pi*(1.8^3); %eyeballed volume of the average organic atom
+h20 = 2.1; %overrounded number for water magnitude
+pts(:,1:3) = round((pts(:,1:3)-offset)/pix+0.5);
+emsz = floor(sz/pix); vol = zeros(emsz);
+solv = (rand(emsz)-0.5)*1*pix^2+(pix^3);
+for i=1:3
+    ix = pts(:,i) < emsz(i) & pts(:,i) > 1; %get points inside the box
+    pts = pts(ix,:); %drop points outside the box
+end
+for i=1:size(pts,1)
+    x=pts(i,1); y=pts(i,2); z=pts(i,3); mag = pts(i,4); %fetch data per atom
+    vol(x,y,z) = vol(x,y,z)+mag;
+    solv(x,y,z) = solv(x,y,z)-avol;
+end
+solv = max(solv,0)/32*h20;
+
+end
+
+
 function vol = ifcn_solv(pix,pts,sz,offset)
 if nargin<4, offset=[0,0,0]; end
 if nargin<3, sz = max(pts,[],1)+pix; end
