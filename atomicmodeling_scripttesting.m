64b240d18d4e81885a2fd2951d133497dab48227
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-13 15:29:19 -0400

    Update atomicmodeling_scripttesting.m

--- atomicmodeling_scripttesting.m
+++ atomicmodeling_scripttesting.m
@@ -79,7 +79,7 @@ alphat = alphaShape(double(pts'),pix*1.2); %shape requires double for some reaso
 ves = 5;
 lipid(1).name = 'lipid'; lipid(1).flags = 'TODO';
 for i=1:ves
-    [pts,perim] = vesgen_sphere(600+randi(200),30+randi(5));
+    [pts,perim] = vesgen_sphere(200+randi(300),30+randi(5));
     lipid(1).perim{1,i} = perim;
     lipid(1).adat{1,i} = pts;
     lipid(1).modelname{i} = append('vesicle');%,string(i));
@@ -90,7 +90,7 @@ layers{1} = lipid;
 %% functionalized model gen part
 boxsize = pix*[200,300,50];
 n = 100; rng(3);
-
+n = [4,100];
 tic
 split = fn_modelgen(layers,boxsize,n);
 toc
@@ -369,7 +369,7 @@ sliceViewer(em+watervol);
 
 %% internal functions
 
-function split = fn_modelgen(layers,boxsize,n)
+function split = fn_modelgen(layers,boxsize,niter)
 dynpts = single(zeros(0,3)); %dynpts = single([-100 -100 -100]);
 %tmp = fieldnames(split);
 %{
@@ -394,6 +394,7 @@ end
 
 for l = 1:numel(layers)
     particles = layers{l};
+    n = niter(l);
 for i=1:n
     if rem(i,n/20)==0; fprintf('%i,',i); end
     
@@ -447,7 +448,7 @@ sf = [(rado^2)/(radi^2),(radi^2)/(rado^2)]/2; %factor to correct for excess inne
 
 %fill space between radii with tons of points
 %ptnum = round(radi*5*(pix^3)*pi^2); %need to actually calculate volume of shell
-shellvol = pi*(rado^3-radi^3)*0.01; %volume of shell in pseudoatoms
+shellvol = pi*(rado^3-radi^3)*0.10; %volume of shell in pseudoatoms
 %ptnum = round( 0.2*shellvol*1^3 )*2; %convert to angstroms, scale to some arbitrary working density
 %frac = [ptnum,ptnum*sf(2),ptnum*sf(1)]; %get fractions of the total to distribute between inner and outer
 rti = round(shellvol*sf(2)); %rto = ptnum-rti; %partition density between inner and outer radii
@@ -470,7 +471,7 @@ ptel = asin(2*rand(ptnum,1)-1); %random elevation angles, corrected for polar de
 [x,y,z] = sph2cart(ptaz,ptel,ptrad); %convert spherical coords to cartesian coords
 pts = [x,y,z];
 %ves = 0;
-n = size(pts,1); ix = randi(n,round(n/500),1);
+n = size(pts,1); ix = randi(n,round(n/20),1);
 %perimix = randperm(n); permix = perimix(1:round(n/400)); perim = pts(perimix,:);
 perim = pts(ix,:);
 in = ones(size(pts,1),1)*60;
