a65ead961e7e4b31923b33c1b9fc556de8a44165
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-20 14:55:58 -0400

    Update atomistic_carbongrid_scripting.m

--- atomistic_carbongrid_scripting.m
+++ atomistic_carbongrid_scripting.m
@@ -8,12 +8,14 @@ hcen = [boxsize(1)/2+randi(120),radius+20+randi(120)]; %offsets for the hole cen
 filmsize = boxsize+pad*2; filmsize(3) = thick;
 
 density = 2.5/(1e8)^3/12*6.022e23; %carbons per A^3, approx 0.1
-ccount = round(density*prod(filmsize)/100);
+ccount = round(density*prod(filmsize)/10);
 carbons = rand(ccount,3).*filmsize-pad;
 carbons(:,3) = carbons(:,3)+(boxsize(3)-thick)/2;
 
 h = sqrt( (carbons(:,1)-hcen(1)).^2 + (carbons(:,2)-hcen(2)).^2 );% +rand(ccount,1)*0;
 carbons = carbons(h>radius,:); %filter out points inside the grid hole
+carbons(:,4) = 2.5088; %scatter val for carbon
 
 %x = 1:100; y = 1.3.^x; plot(x,y);
-plot3(carbons(:,1),carbons(:,2),carbons(:,3),'.'); axis equal
+%plot3(carbons(:,1),carbons(:,2),carbons(:,3),'.'); axis equal
+[vol] = helper_atoms2vol(pix,carbons,boxsize); sliceViewer(vol);
