f9ebd0fe5074fef515d9803aaf46e3db295b32b2
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-20 14:16:39 -0400

    Update atomistic_carbongrid_scripting.m

--- atomistic_carbongrid_scripting.m
+++ atomistic_carbongrid_scripting.m
@@ -1,6 +1,12 @@
 %% atomistic carbon grid generator
 pix = 12;
 boxsize = pix*[400,500,50];
+thick = 1e4; %hole radius
+radius = 150; %carbon thickness
 
 
+
+
+
+%x = 1:100; y = 1.3.^x; plot(x,y);
 
