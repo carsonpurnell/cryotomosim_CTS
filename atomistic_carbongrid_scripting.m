b66ce047ec00a2a13573d7d13d283bb4a738b1a3
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-20 14:50:02 -0400

    Update atomistic_carbongrid_scripting.m

--- atomistic_carbongrid_scripting.m
+++ atomistic_carbongrid_scripting.m
@@ -1,10 +1,10 @@
 %% atomistic carbon grid generator
 pix = 12;
-boxsize = pix*[400,500,50];
+boxsize = pix*[300,500,50];
 pad = [pix,pix,0];
 radius = 1e4; %hole radius
 thick = 150; %carbon thickness
-hcen = [size(boxsize,1)/2+randi(50),radius+20+randi(10)-pix*1]; %offsets for the hole center
+hcen = [boxsize(1)/2+randi(120),radius+20+randi(120)]; %offsets for the hole center
 filmsize = boxsize+pad*2; filmsize(3) = thick;
 
 density = 2.5/(1e8)^3/12*6.022e23; %carbons per A^3, approx 0.1
@@ -12,8 +12,8 @@ ccount = round(density*prod(filmsize)/100);
 carbons = rand(ccount,3).*filmsize-pad;
 carbons(:,3) = carbons(:,3)+(boxsize(3)-thick)/2;
 
-h = sqrt( (pts(1,:)-hcen(1)).^2 + (pts(2,:)-hcen(2)).^2 ) +rand(ccount,1)*0;
-carbons2 = round(carbons(h>radius,:)+0.5); %filter out points inside the grid hole
+h = sqrt( (carbons(:,1)-hcen(1)).^2 + (carbons(:,2)-hcen(2)).^2 );% +rand(ccount,1)*0;
+carbons = carbons(h>radius,:); %filter out points inside the grid hole
 
 %x = 1:100; y = 1.3.^x; plot(x,y);
-plot3(carbons(:,1),carbons(:,2),carbons(:,3)); axis equal
+plot3(carbons(:,1),carbons(:,2),carbons(:,3),'.'); axis equal
