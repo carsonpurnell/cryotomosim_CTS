f6c0ff33d077cb968dc32499cf5e861b401050e9
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-21 12:35:19 -0400

    Update atomicmodeling_scripttesting.m

--- atomicmodeling_scripttesting.m
+++ atomicmodeling_scripttesting.m
@@ -101,7 +101,7 @@ toc
 %% function for vol, atlas, and split generation + water solvation
 [vol,solv,atlas,splitvol] = helper_atoms2vol(pix,split,boxsize);
 sliceViewer(vol+solv);
-WriteMRC(vol+solv,14,'atomicmodtest_lipid2.mrc');
+%WriteMRC(vol+solv,14,'atomicmodtest_lipid2.mrc');
 
 
 %% randomly add to the points and concatenate them into a list
