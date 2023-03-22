8776edd9b4af72b08a0c8f14edc6269366c4caed
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-22 14:01:05 -0400

    Update atomistic_carbongrid_scripting.m

--- atomistic_carbongrid_scripting.m
+++ atomistic_carbongrid_scripting.m
@@ -1,6 +1,6 @@
 %% atomistic carbon grid generator
 pix = 12;
-boxsize = pix*[300,500,50];
+boxsize = pix*[300,400,50];
 pad = [pix,pix,0];
 radius = 1e4; %hole radius
 thick = 150; %carbon thickness
@@ -8,14 +8,14 @@ hcen = [boxsize(1)/2+randi(120),radius+20+randi(120)]; %offsets for the hole cen
 filmsize = boxsize+pad*2; filmsize(3) = thick;
 
 density = 2.5/(1e8)^3/12*6.022e23; %carbons per A^3, approx 0.1
-atomfrac = 5;
+atomfrac = 2;
 ccount = round(density*prod(filmsize)/atomfrac);
 carbons = rand(ccount,3).*filmsize-pad;
 carbons(:,3) = carbons(:,3)+(boxsize(3)-thick)/2;
 
 h = sqrt( (carbons(:,1)-hcen(1)).^2 + (carbons(:,2)-hcen(2)).^2 );% +rand(ccount,1)*0;
 carbons = carbons(h>radius,:); %filter out points inside the grid hole
-carbons(:,4) = 2.5088*atomfrac; %scatter val for carbon
+carbons(:,4) = 2.5088*atomfrac/2; %scatter val for carbon
 
 %x = 1:100; y = 1.3.^x; plot(x,y);
 %plot3(carbons(:,1),carbons(:,2),carbons(:,3),'.'); axis equal
