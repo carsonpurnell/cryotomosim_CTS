0c3d2aff6cc3a863998b1fffacb91531bad7bc73
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-03-27 15:18:21 -0400

    Update helper_atoms2vol.m

--- helper_atoms2vol.m
+++ helper_atoms2vol.m
@@ -32,13 +32,15 @@ for j=1:s
         p = pts;
     end
     if size(p,2)<4, p(:,4)=1; end %intensity==1 if not provided in 4th column
-    p(:,1:3) = round((p(:,1:3)-offset)/pix+0.5);
+    m = p(:,1); p = p(:,1:3);
+    p = round( (p-offset)/pix+0.5 );
+    %p(:,1:3) = round((p(:,1:3)-offset)/pix+0.5);
     for i=1:3
         ix = p(:,i) <= emsz(i) & p(:,i) >= 1; %get points inside the box
         p = p(ix,:); %drop points outside the box
     end
     for i=1:size(p,1)
-        x=p(i,1); y=p(i,2); z=p(i,3); mag = p(i,4); %fetch data per atom
+        x=p(i,1); y=p(i,2); z=p(i,3); mag = m(i); %fetch data per atom
         split(x,y,z,j) = split(x,y,z,j)+mag;
         solv(x,y,z) = solv(x,y,z)-avol*(rand*.4+0.8);
     end
