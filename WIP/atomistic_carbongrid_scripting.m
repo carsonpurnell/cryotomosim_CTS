8c8e5fc48895ed497c1ca20b3f216c7fb1eeca93
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-05-30 10:24:47 -0400

    Update atomistic_carbongrid_scripting.m

--- WIP/atomistic_carbongrid_scripting.m
+++ WIP/atomistic_carbongrid_scripting.m
@@ -37,7 +37,7 @@ vec = randn(size(ps)); mag = rand(size(ps,1),1)*60;
 vec = mag.*vec./vecnorm(vec,2,2);
 
 sh = alphaShape(ps+vec,40);
-d2 = 2.0/12*(1e8^-3)*6.022e23; %carbons per A^3, approx 0.1
+d2 = 2.3/12*(1e8^-3)*6.022e23; %carbons per A^3, approx 0.1
 atomfrac = 1;
 vp = randtess(d2/atomfrac,sh,'v'); %fewer pts with more density for speed during modelgen
 vp(:,4) = 2.5088*4;
