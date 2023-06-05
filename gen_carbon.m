2fa0e90aedbd61635f469c9459ea551c5b3447bf
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-06-04 21:59:01 -0400

    Update gen_carbon.m

--- gen_carbon.m
+++ gen_carbon.m
@@ -1,21 +1,30 @@
-function [carbon] = gen_carbon(vol,pix)
-
+function [carbon] = gen_carbon(vol,pix,opt)
 
 arguments
     vol (1,3)
     pix = [] %empty or 0 returns atoms, real value returns the volume
+    opt.thick = 150
+    opt.radius = 1e4
 end
 
 
 %% atomistic carbon grid generator
 %pix = 12;
 %boxsize = pix*[200,300,50];
-boxsize = vol;
-pad = [50,50,0];
-radius = 1e4; %hole radius
-thick = 150; %carbon thickness
-hcen = [boxsize(1)/2+randi(600)-300,radius+30+randi(200)]; %offsets for the hole center
-filmsize = boxsize+pad*2; filmsize(3) = thick;
+%boxsize = vol;
+
+if pix>0
+    vol = vol*pix; %get atomistic scale when working from pixels
+end
+
+%{
+pad = [20,20,0]; %padding to avoid edge effects
+radius = opt.radius; %hole radius
+thick = opt.thick; %carbon thickness
+
+hcen = [vol(1)/2+randi(600)-300,opt.radius+30+randi(200)]; %offsets for the hole center
+filmsize = vol+pad*2; filmsize(3) = opt.thick;
+%}
 
 %{
 %% direct flat pt gen
@@ -35,11 +44,12 @@ c1 = helper_atoms2vol(pix,carbons,boxsize,[0,0,0]);
 
 %% comparison fill alphashape density
 
-psn = round(prod(filmsize)/18000); %
+%{
+psn = round(prod(filmsize)/18000); % 18000 just looks nice and is fast, not evaluated or hypothesis-driven
 ps = rand(psn,3).*filmsize-pad;
-h = sqrt( (ps(:,1)-hcen(1)).^2 + (ps(:,2)-hcen(2)).^2 );% +rand(ccount,1)*0;
-ps = ps(h>radius,:);
-ps(:,3) = ps(:,3)+(boxsize(3)-thick)/2;
+h = sqrt( (ps(:,1)-hcen(1)).^2 + (ps(:,2)-hcen(2)).^2 ); %find points inside hole
+ps = ps(h>opt.radius,:); %remove points in hole
+ps(:,3) = ps(:,3)+(vol(3)-opt.thick)/2; %adjust grid to the center of the volume
 
 %sh = alphaShape(carbons(:,1:3),pix); %way too many points
 %[~,ps] = boundaryFacets(sh);
@@ -47,6 +57,10 @@ vec = randn(size(ps)); mag = rand(size(ps,1),1)*60;
 vec = mag.*vec./vecnorm(vec,2,2);
 
 sh = alphaShape(ps+vec,40);
+%}
+sh = carbonshape(vol,opt);
+
+
 d2 = 2.0/12*(1e8^-3)*6.022e23; %carbons per A^3, approx 0.1
 atomfrac = 4;
 vp = randtess(d2/atomfrac*3,sh,'v'); %fewer pts with more density for speed during modelgen
@@ -54,9 +68,33 @@ vp(:,4) = 2.5088*1*atomfrac;
 
 carbon = vp;
 if pix>0
-    carbon = helper_atoms2vol(pix,vp,boxsize,[0,0,0]);
+    carbon = helper_atoms2vol(pix,vp,vol,[0,0,0]);
 end
 
 
+
+end
+
+function edge = carbonshape(vol,opt)
+
+pad = [20,20,0]; %padding to avoid edge effects
+%radius = opt.radius; %hole radius
+%thick = opt.thick; %carbon thickness
+
+hcen = [vol(1)/2+randi(600)-300,opt.radius+30+randi(200)]; %offsets for the hole center
+filmsize = vol+pad*2; filmsize(3) = opt.thick;
+
+psn = round(prod(filmsize)/18000); % 18000 just looks nice and is fast, not evaluated or hypothesis-driven
+ps = rand(psn,3).*filmsize-pad;
+h = sqrt( (ps(:,1)-hcen(1)).^2 + (ps(:,2)-hcen(2)).^2 ); %find points inside hole
+ps = ps(h>opt.radius,:); %remove points in hole
+ps(:,3) = ps(:,3)+(vol(3)-opt.thick)/2; %adjust grid to the center of the volume
+
+%sh = alphaShape(carbons(:,1:3),pix); %way too many points
+%[~,ps] = boundaryFacets(sh);
+vec = randn(size(ps)); mag = rand(size(ps,1),1)*60;
+vec = mag.*vec./vecnorm(vec,2,2);
+
+edge = alphaShape(ps+vec,40);
 
 end
