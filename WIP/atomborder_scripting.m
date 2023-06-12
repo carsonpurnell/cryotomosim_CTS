05c550836b35497de5b78eedee248e867a56b132
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-06-12 10:53:26 -0400

    Update atomborder_scripting.m

--- WIP/atomborder_scripting.m
+++ WIP/atomborder_scripting.m
@@ -25,15 +25,21 @@ end
 
 [x,y,z] = ind2sub(size(vol),find(vol>0));
 b = [x,y,z]*pix;
+d1 = 42;
 
-bshell = repmat(b,[10,1]);
-d = 20;
-vec = randn(size(bshell)); vec = d*vec./vecnorm(vec,2,2);
+n = size(b,1); ix = randperm(n); ix = ix(1:round(n/5));
+b = b(ix,:);
+vec = randn(size(b)); vec = d1*vec./vecnorm(vec,2,2);
+b = b+vec;
+
+bshell = repmat(b,[4,1]);
+d2 = 16;
+vec = randn(size(bshell)); vec = d2*vec./vecnorm(vec,2,2);
 bshell = bshell+vec;
 
 %n = size(b,1); ix = randperm(n); ix = ix(1:round(n/10));
 
-sh = alphaShape(bshell,pix*5);
+sh = alphaShape(bshell,pix*4);
 plot(sh)
 %plot3(x,y,z,'.')
 %plot3(qq(:,1),qq(:,2),qq(:,3),'.')
