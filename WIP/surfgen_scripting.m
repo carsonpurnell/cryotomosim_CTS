6d20bf8717880dea024322f0df38e403d113750d
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-06-12 14:03:17 -0400

    Update surfgen_scripting.m

--- WIP/surfgen_scripting.m
+++ WIP/surfgen_scripting.m
@@ -13,7 +13,7 @@ i = min(X-1,sz(1)-X+1); j = min(Y-1,sz(2)-Y+1);
 H = exp(-.5*(i.^2+j.^2)/n^2);
 Z = real(ifft2(H.*fft2(randn(sz)))); % 0-centered, approximately normal
 
-pts = [X(:),Y(:),Z(:)*1e3];
+pts = [X(:),Y(:),Z(:)*2e3];
 n = size(pts,1); ix = randperm(n); ix = ix(1:round(n/10));
 pts = pts(ix,:);
 pts(:,1:2) = pts(:,1:2)-pad;
