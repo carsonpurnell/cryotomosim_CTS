f563b0b548fb6c1009e0767a001251c38280d865
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-06-12 11:47:04 -0400

    Update surfgen_scripting.m

--- WIP/surfgen_scripting.m
+++ WIP/surfgen_scripting.m
@@ -2,6 +2,9 @@
 function pts = gen_surf(sz,n)
 %sz = [400,300];
 %n = 2.5; % noise magnitude
+len = max(sz);
+%padding
+%pruning square back down to target size
 
 [X,Y] = ndgrid(1:sz(1),1:sz(2));
 i = min(X-1,sz(1)-X+1); j = min(Y-1,sz(2)-Y+1);
@@ -9,7 +12,7 @@ H = exp(-.5*(i.^2+j.^2)/n^2);
 Z = real(ifft2(H.*fft2(randn(sz)))); % 0-centered, approximately normal
 
 pts = [X(:),Y(:),Z(:)*1e3];
-n = size(pts,1); ix = randperm(n); ix = ix(1:round(n/10));
+n = size(pts,1); ix = randperm(n); ix = ix(1:round(n/100));
 pts = pts(ix,:);
 
 %figure();
