b56b0b501faaea06f25e07f5ca70ae1732f062f4
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-06-12 11:22:13 -0400

    Update surfgen_scripting.m

--- WIP/surfgen_scripting.m
+++ WIP/surfgen_scripting.m
@@ -1,6 +1,7 @@
 %% wavy surface generator
-sz = [400,300];
-n = 2.5; % noise magnitude
+function pts = gen_surf(sz,n)
+%sz = [400,300];
+%n = 2.5; % noise magnitude
 
 [X,Y] = ndgrid(1:sz(1),1:sz(2));
 i = min(X-1,sz(1)-X+1); j = min(Y-1,sz(2)-Y+1);
@@ -9,7 +10,8 @@ Z = real(ifft2(H.*fft2(randn(sz)))); % 0-centered, approximately normal
 
 pts = [X(:),Y(:),Z(:)*1e3];
 n = size(pts,1); ix = randperm(n); ix = ix(1:round(n/10));
-pts2 = pts(ix,:);
+pts = pts(ix,:);
 
 %figure();
 %plot3(pts2(:,1),pts2(:,2),pts2(:,3),'.'); axis equal;
+end
