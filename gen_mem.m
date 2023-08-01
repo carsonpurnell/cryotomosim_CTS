a8677fe567e7cf9b409abb09e2d2805c64f3c2c8
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-01 13:51:02 -0400

    Update gen_mem.m

--- gen_mem.m
+++ gen_mem.m
@@ -5,7 +5,7 @@ arguments
     sz
     pix = []
     sp = 0.6+rand*0.4
-    thick = 24+randi(6)
+    thick = 26+randi(6)
 end
 vol = 0;
 
@@ -99,11 +99,11 @@ shell = alphaShape(shellpts,24);
 end
 
 function [pts,head,tail] = shell2pts(shell,atomfrac)
-surfvar = 12;
+surfvar = 10;
 %atomfrac = 2; %make operable? 4 super rough at higher pixel sizes, but 1 very slow for atomic gen
 
 tail = randtess(0.03/atomfrac,shell,'v'); % need larger hydrophobic dict
-head = randtess(1/atomfrac,shell,'s'); %was 20,testing for less bilayer
+head = randtess(1.1/atomfrac,shell,'s'); %was 20,testing for less bilayer
 
 vec = randn(size(head));
 spd = rand(size(vec,1),1)*surfvar+0;
