81bdeb5a0a7bf8339f096156ca8c4994c27ae189
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-20 20:12:07 -0400

    Update filament_polym_linear_script.m

--- WIP/filament_polym_linear_script.m
+++ WIP/filament_polym_linear_script.m
@@ -4,14 +4,16 @@
 % filaments rotate in the wrong direction - XY inversion for everything
 % is this from pdb2vol? in normal cts models too
 pix = 6; ori = [0,0,1];
-%dat = helper_pdb2vol('actin_mono_fil2.cif',pix,0,1,0); ang = -166.15; step = 27.3*1; flex = 12; minL=20;
+dat = helper_pdb2vol('actin_mono_fil2.cif',pix,0,1,0); ang = -166.15; step = 27.3*1; flex = 12; minL=20;
 %dat = helper_pdb2vol('MTring2.cif',pix,0,1,0); ang = 0; step = 85; flex = 5; minL=8;
-dat = helper_pdb2vol('cof_reZ.pdb',pix,0,1,0); ang = -160; step = 24*1; flex = 12*1.0; minL = 15;
+%dat = helper_pdb2vol('cof_reZ.pdb',pix,0,1,0); ang = -160; step = 24*1; flex = 10*1.0; minL = 15;
 %part of errors is from non-centering, so wildly wrong Z axis borks everything
 %measure center and move z d models # to z-flatten things seems to fix it well enough
 %minimum repeat for each filament type, maximum length? or default very overlong loop?
 sumv = sum(cat(4,dat{:}),4);
 r = max(size(sumv,[1,2]))/3-4; %find approximate maximum radius for bwdist comparison efficiency
+mono.vol = dat; mono.sum = sum(cat(4,dat{:}),4);
+mono.ang = ang; mono.step = step; mono.flex = flex; mono.minlength = minL;
 %
 %cof dimer is slightly broken, too close to centroid? need to remake it without changing xy location
 %rng(3)
@@ -99,7 +101,7 @@ fvol = vol*0;
 end
 %}
 profile on
-ovol = vol_fill_fil(mvol,con,pix,sumv,step,ang,flex,minL);
+ovol = vol_fill_fil(mvol,con,pix,mono);
 sliceViewer(ovol); 
 %profile viewer
 %%
