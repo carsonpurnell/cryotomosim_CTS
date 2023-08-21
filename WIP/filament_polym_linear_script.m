a17c6d5d3dd0a0501d0c5fde84f0471d96218709
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-20 20:43:59 -0400

    Update filament_polym_linear_script.m

--- WIP/filament_polym_linear_script.m
+++ WIP/filament_polym_linear_script.m
@@ -1,11 +1,15 @@
+%try an atom-based version, could be much faster (and less rounding jigger)
+%only need to remake model trees if a new filament was placed, no weirdness with CoM
+%requires atomistic grid and membrane though
 
 %% integrated filament walk - vol-based version
 
-% filaments rotate in the wrong direction - XY inversion for everything
+% filaments rotate in the wrong direction - XY inversion for everything?
 % is this from pdb2vol? in normal cts models too
 pix = 10; ori = [0,0,1];
-dat = helper_pdb2vol('actin_mono_fil2.cif',pix,0,1,0); ang = -166.15; step = 27.3*1; flex = 12; minL=20;
+%can save with arbitrary file extensions - .fil or similar. just need to load with load(fil,'-mat')
 %dat = helper_pdb2vol('MTring2.cif',pix,0,1,0); ang = 0; step = 85; flex = 5; minL=8;
+dat = helper_pdb2vol('actin_mono_fil2.cif',pix,0,1,0); ang = -166.15; step = 27.3*1; flex = 12; minL=20;
 %dat = helper_pdb2vol('cof_reZ.pdb',pix,0,1,0); ang = -160; step = 24*1; flex = 10*1.0; minL = 15;
 %part of errors is from non-centering, so wildly wrong Z axis borks everything
 %measure center and move z d models # to z-flatten things seems to fix it well enough
@@ -14,8 +18,6 @@ sumv = sum(cat(4,dat{:}),4);
 r = max(size(sumv,[1,2]))/3-4; %find approximate maximum radius for bwdist comparison efficiency
 mono.vol = dat; mono.sum = sum(cat(4,dat{:}),4);
 mono.ang = ang; mono.step = step; mono.flex = flex*pi/180; mono.minlength = minL;
-%
-%cof dimer is slightly broken, too close to centroid? need to remake it without changing xy location
 %rng(3)
 mvol = gen_memvol(zeros(400,300,50),pix,2,5)*1;
 %flex = flex*pi/180; %ang = ang*pi/180; %vol method is degree based
@@ -101,7 +103,7 @@ fvol = vol*0;
 end
 %}
 profile on
-ovol = vol_fill_fil(mvol,con,pix,mono);
+ovol = vol_fill_fil(mvol,con,pix,mono); %it works, it's just so slow
 sliceViewer(ovol); 
 %profile viewer
 %%
