5e19ae4c10d04a734f604f717c2e46262249ce0d
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-22 14:47:58 -0400

    Update filament_polym_linear_script.m

--- WIP/filament_polym_linear_script.m
+++ WIP/filament_polym_linear_script.m
@@ -8,33 +8,42 @@
 % filaments rotate in the wrong direction - XY inversion for everything?
 % is this from pdb2vol? in normal cts models too
 pix = 8; ori = [0,0,1];
-input = 'actin_mono_fil2.cif'; ang = -166.15; step = 27.3; flex = 12; minL = 20;
-%input = 'MTring2.cif'; ang = 0; step = 85; flex = 5; minL = 8;
-%input = 'cof_fix3.cif'; ang = -160; step = 24; flex = 10; minL = 15;
+%input = 'actin_mono_fil2.cif'; prop = [-166.15,27.3,12,20];
+input = 'MTring2.cif'; prop = [0,85,5,8];
+%input = 'cof_fix3.cif'; prop = [-162,24,10,20];
+
+%input = 'actin_mono_fil2.cif'; %ang = -166.15; step = 27.3; flex = 12; minL = 20;
+%input = 'MTring2.cif'; %ang = 0; step = 85; flex = 5; minL = 8;
+%input = 'cof_fix3.cif'; %ang = -160; step = 24; flex = 10; minL = 15;
 %dat = helper_pdb2vol(input,pix,0,1,0); 
 %dat = helper_pdb2vol('MTring2.cif',pix,0,1,0); ang = 0; step = 85; flex = 5; minL=8;
 %dat = helper_pdb2vol('cof_reZ2.pdb',pix,0,1,0); ang = -160; step = 24*1; flex = 10*1.0; minL = 15;
 %can save with arbitrary file extensions - .fil or similar. just need to load with load(fil,'-mat')
 
-dat = helper_pdb2dat(input,pix,0,1,0);
+monomer = helper_filmono(input,pix,prop);
+
+%dat = helper_pdb2dat(input,pix,0,1,0);
 %sumv = sum(cat(4,dat{:}),4);
 %need dictionary function to transform between atom Z values and scattering magnitudes
-sumv = helper_atoms2vol(pix,dat.adat,[0,0,0])*3; %lower intensity from scatter v Z
+%sumv = helper_atoms2vol(pix,dat.adat,[0,0,0])*3; %lower intensity from scatter v Z
 %atoms2vol output centered on 0,0,0?
 %part of errors is from non-centering, so wildly wrong Z axis borks everything
 %measure center and move z d models # to z-flatten things seems to fix it well enough
 %minimum repeat for each filament type, maximum length? or default very overlong loop?
 %
+%{
 %r = max(size(sumv,[1,2]))/3-4; %find approximate maximum radius for bwdist comparison efficiency
 %mono.vol = dat; 
 mono.sum = sumv;
 mono.ang = ang; mono.step = step; mono.flex = flex;%*pi/180; 
 mono.minlength = minL;
 %an minL be derived from step size and flexibility in some way?
-rng(3)
-mvol = gen_memvol(zeros(400,300,50),pix,2,5)*1;
 %flex = flex*pi/180; %ang = ang*pi/180; %vol method is degree based
 %
+%}
+rng(3)
+mvol = gen_memvol(zeros(400,300,50),pix,2,5)*1;
+
 con = helper_constraints(mvol*0,'  &')*pix^2.5;
 %{
 for nn=1:10
@@ -116,7 +125,7 @@ fvol = vol*0;
 end
 %}
 profile on
-ovol = vol_fill_fil(mvol,con,pix,mono); %it works, it's just so slow
+ovol = vol_fill_fil(mvol,con,pix,monomer); %it works, it's just so slow
 sliceViewer(ovol); 
 profile viewer
 %%
