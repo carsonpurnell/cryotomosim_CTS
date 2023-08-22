dbc1e0eca1ab12e33a87074fcd588f797afedf1c
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-22 14:48:02 -0400

    Update helper_filmono.m

--- WIP/helper_filmono.m
+++ WIP/helper_filmono.m
@@ -1,14 +1,13 @@
-function particle = helper_filmono(input,prop)
-%
+function particle = helper_filmono(input,pix,prop)
+% particle = helper_filmono(input,prop)
 %
 %
 %
 arguments
     input
+    pix
     prop = 'gui'
 end
-%}
-%input = 'gui';
 %input = 'actin_mono_fil2.cif'; 
 %ang = -166.15; step = 27.3; flex = 12; minL = 20;
 %prop = [-166.15,27.3,12,20];
@@ -17,7 +16,6 @@ if strcmp(input,'gui')
     input = fullfile(path,input); %parse filename for functional off-path use
 end
 %if ~isfile(input), error('file does not appear to exist - use "gui" for off-path files'); end
-%prop = 'gui';
 if strcmp(prop,'gui') || numel(prop)~=4
     prompt = {'Helical angle','Monomer step size','Filament flexibility','Minimum length'};
     tmp = inputdlg(prompt,'filament properties'); %present GUI dialogue for manual input
@@ -27,10 +25,14 @@ if strcmp(prop,'gui') || numel(prop)~=4
     end
 end
 
+
+
 %do the file loading
 dat = helper_pdb2dat(input,2,0,1,0);
+sum = helper_atoms2vol(pix,dat.adat,[0,0,0])*3;
 particle.name = dat.name;
 particle.filprop = prop;
+particle.sum = sum;
 particle.adat = dat.adat;
 particle.modelname = dat.modelname;
 
