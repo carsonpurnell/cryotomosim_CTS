d1aef635de46358dba319f68e8abe997701b7ce8
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-27 16:37:38 -0400

    Update helper_filinput.m

--- WIP/helper_filinput.m
+++ WIP/helper_filinput.m
@@ -1,16 +1,26 @@
-function particles = helper_filinput(pix,input)
+function particle = helper_filinput(pix,list)
 
 arguments
     pix
-    input = 'gui'
+    list = 'gui'
 end
 
-elseif strcmp(list,'gui')
-    [list, path] = uigetfile({'*.pdb;*.pdb1;*.mrc;*.cif;*.mmcif'},'Select input files','MultiSelect','on');
+if strcmp(list,'gui') %select the files themselves
+    [list, path] = uigetfile({'*.pdb;*.pdb1;*.mrc;*.cif;*.mmcif';'*.fil'},'Select input files','MultiSelect','on');
     if numel(string(list))==1, list={list}; end
     if ~iscell(list) || numel(list)==0, error('No files selected, aborting.'); end
     for i=1:numel(list) %make the list full file paths rather than just names so it works off-path
         list{i} = fullfile(path,list{i}); 
+    end
+end
+
+for i=1:numel(list) %loop through and parse inputs through filmono for structs and directly for .fil
+    [~,~,ext] = fileparts(list{i});
+    switch ext
+        case '.fil'
+            qq = load(list{i},'-mat'); particle(i) = qq.monomer; %#ok<AGROW>
+        case {'.cif','.mmcif','.pdb','.pdb1'}
+            particle(i) = helper_filmono(list{i},pix); %#ok<AGROW>
     end
 end
 
