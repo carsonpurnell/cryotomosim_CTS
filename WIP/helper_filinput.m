96863eb7fd8b62ababc3e507c9a89c6e17a39062
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-27 17:53:27 -0400

    Update helper_filinput.m

--- WIP/helper_filinput.m
+++ WIP/helper_filinput.m
@@ -6,7 +6,7 @@ arguments
 end
 
 if strcmp(list,'gui') %select the files themselves
-    [list, path] = uigetfile({'*.pdb;*.pdb1;*.mrc;*.cif;*.mmcif';'*.fil'},'Select input files','MultiSelect','on');
+    [list, path] = uigetfile({'*.pdb;*.pdb1;*.mrc;*.cif;*.mmcif;*.fil'},'Select input files','MultiSelect','on');
     if numel(string(list))==1, list={list}; end
     if ~iscell(list) || numel(list)==0, error('No files selected, aborting.'); end
     for i=1:numel(list) %make the list full file paths rather than just names so it works off-path
