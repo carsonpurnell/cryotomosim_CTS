1dc915cbc76bc7fe3714000cab440a566e31b1d0
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-08-30 13:58:32 -0400

    Update helper_filinput.m

--- WIP/helper_filinput.m
+++ WIP/helper_filinput.m
@@ -18,7 +18,8 @@ for i=1:numel(list) %loop through and parse inputs through filmono for structs a
     [~,~,ext] = fileparts(list{i});
     switch ext
         case '.fil'
-            qq = load(list{i},'-mat'); particle(i) = qq.monomer;  %#ok<AGROW>
+            qq = load(list{i},'-mat'); qq.monomer.perim = 0;
+            particle(i) = qq.monomer;  %#ok<AGROW>
             [sum,~,~,split] = helper_atoms2vol(pix,particle(i).adat,[0,0,0]); 
             %if ~iscell(split)
             convfac = 3; %approximate conversion factor from scatter value to Z numbers
@@ -30,6 +31,10 @@ for i=1:numel(list) %loop through and parse inputs through filmono for structs a
         case {'.cif','.mmcif','.pdb','.pdb1'}
             particle(i) = helper_filmono(list{i},pix); %#ok<AGROW>
     end
+    tmp = vertcat(particle(i).adat{:}); %cat all partitions for perim testing
+    alphat = alphaShape(double(tmp(:,1:3)),12); 
+    [~,p] = boundaryFacets(alphat);
+    particle(i).perim = p; %#ok<AGROW>
 end
 
 end
