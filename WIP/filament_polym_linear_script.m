2d83a521be9ae516f510de82169b9b1d15483c3b
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-09-05 13:51:33 -0400

    Update filament_polym_linear_script.m

--- WIP/filament_polym_linear_script.m
+++ WIP/filament_polym_linear_script.m
@@ -1,5 +1,4 @@
 
-
 %% integrated polymer walk - atomistic
 profile on
 pix = 8;
@@ -617,6 +616,26 @@ for i=1:4
     pts = [pts;ptsb-[0,0,dl*i]]; pts = [pts;ptst+[0,0,dl*i]];
 end
 con = pts;
+end
+
+function pts = internal_gen_atomborder(sz,n,sc)
+%sz = [400,300];
+%n = 2.5; % noise magnitude
+%sc = 500; % scale of Z noise
+len = max(sz);
+pad = 20; %padding - scale by input size maybe? prune afterward?
+sz = sz+pad*2;
+
+ptsep = 3;
+[X,Y] = ndgrid(1:ptsep:sz(1),1:ptsep:sz(2));
+i = min(X-1,sz(1)-X+1); j = min(Y-1,sz(2)-Y+1);
+H = exp(-.5*(i.^2+j.^2)/n^2);
+Z = real(ifft2(H.*fft2(randn(size(X))))); % 0-centered, approximately normal
+
+pts = [X(:),Y(:),Z(:)*sc];
+n = size(pts,1); ix = randperm(n); ix = ix(1:round(n/5));
+pts = pts(ix,:);
+pts(:,1:2) = pts(:,1:2)-pad;
 end
 
 function err = proxtest(c,pts,tol)
