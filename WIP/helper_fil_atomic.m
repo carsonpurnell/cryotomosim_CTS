5f409a58f61e85e08bfdced72048784706ab3c65
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-09-13 10:48:45 -0400

    Update helper_fil_atomic.m

--- WIP/helper_fil_atomic.m
+++ WIP/helper_fil_atomic.m
@@ -404,22 +404,23 @@ if ~isempty(ix) %this thing is taking SO VERY LONG, need more pre-optimization
 end
 end
 
+%{
 function vec = randv(row,col,ax,ang) %ang IN RADIANS
+%asin to correct for center bias?
 rax = randvec(row,col); %random vectors - finished or to cross with the center axis
 if isempty(ax) && isempty(ang)
     vec=rax; %if no ax/ang, end and return random vecs
 else
     if isempty(ax), ax = randvec(1,3); end %if no axis given, randomize one
-    %if ang is 1x2, use 1st for min 2nd max?
-    if numel(ang)==1, ang(2)=ang(1); ang(1)=0; end
-    ang(2) = ang(2)-ang(1); %store difference from min for simpler following code
-    nrep = row/size(ax,1); %number of replicates needed to match matrix size for cross
+    if numel(ang)==1, ang(2)=ang(1); ang(1)=0; end %if only 1 ang, use as max against min 0
+    %ang(2) = ang(2)-ang(1); %store difference from min for simpler following code
+    %nrep = row/size(ax,1); %number of replicates needed to match matrix size for cross
     ax = ax/norm(ax); %unitize target vector to avoid miscalculation
-    rotax = cross(repmat(ax,nrep,1),rax); %compute orthogonal axes to rotate
+    rotax = cross(repmat(ax,row/size(ax,1),1),rax); %compute orthogonal axes to rotate
     rotax = (rotax'./vecnorm(rotax'))'; %unitize orthogonal axes
     vec = zeros(row,col);
     for i=1:row %loop because matrix multiplication can't be vectorized?
-        R = rotmat(rotax(i,:),rand*ang(2)+ang(1)); %rotation matrix for the point
+        R = rotmat(rotax(i,:),rand*(ang(2)-ang(1))+ang(1)); %rot matrix about axis, random angle within range
         vec(i,:) = ax*R; %vector of rotated point away form cone center
     end
 end
@@ -429,6 +430,7 @@ function [vec] = randvec(row,col)
 vec = randn(row,col); %random normal numbers for evenly-distributed vector directions
 vec = (vec'./vecnorm(vec'))'; %unitize vectors to length 1 for sphere vectors
 end
+%}
 
 function t = rotmat(ax,rad)
 ax = ax/norm(ax);
