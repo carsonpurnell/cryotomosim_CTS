baf20734fde96be1294a51358e3529e9a7695eb4
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-09-19 09:53:52 -0400

    Update util_loadfiles.m

--- util_loadfiles.m
+++ util_loadfiles.m
@@ -4,18 +4,13 @@ function list = util_loadfiles(filter,prompt,multi)
 %filter: file extension filter string, in the format '*.mmcif;*.mat'  defaults to '*.mat'
 %prompt: window display title as string, default 'Select input structure files'
 %multi: exact number of input files, or [min,max]. empty vector [] allows any number, and is default
-%
 arguments
     filter = '*.mat' %default backstop filetype filter
     prompt = 'Select input files' %window display title
     multi = []; %empty vector to multiselect
 end
-
-if exist('uipickfiles','file')==2 %&& strcmp(list,'gui') % preferred method of using GUI to find target files
-    %filter = replace(filter,'*','\'); filter = replace(filter,';','$|'); %replace separators
-    %filter = replace(replace(filter,'*','\'),';','$|'); %append maybe important last symbol
+if exist('uipickfiles','file')== 2 %&& strcmp(list,'gui') %better FEX browser UI
     filter = append(replace(replace(filter,'*','\'),';','$|'),'$'); %parse filter
-    %list = uipickfiles('REFilter','\.mrc$|\.pdb$|\.mat$|\.pdb1$|\.cif$|\.mmcif$','Prompt',prompt); 
     list = uipickfiles('REFilter',filter,'Prompt',prompt,'NumFiles',multi); 
     if ~iscell(list) || numel(list)==0, error('No files selected, aborting.'); end
 else%if strcmp(list,'gui')
@@ -23,7 +18,7 @@ else%if strcmp(list,'gui')
     [list, path] = uigetfile({filter},prompt,'MultiSelect',multi);
     if numel(string(list))==1, list={list}; end %fix single selection not being in a cell
     if ~iscell(list) || numel(list)==0, error('No files selected, aborting.'); end
-    for i=1:numel(list) %make the list full file paths rather than just names so it works off-path
+    for i=1:numel(list) %append paths to allow reliable off-path use of files
         list{i} = fullfile(path,list{i}); 
     end
 end
