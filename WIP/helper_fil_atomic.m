db85f47faed92e65a7c51c6fe99667419458f379
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-10-02 14:12:41 -0400

    Update helper_fil_atomic.m

--- WIP/helper_fil_atomic.m
+++ WIP/helper_fil_atomic.m
@@ -46,7 +46,7 @@ for i=1:iters
                     com = pos-vecc*mono.filprop(2)/2; %translation vector, halfway along step vector
                     ovcheck = mono.perim*r1*r2+com; %apply rotations (order dependent!) and translation
                     err = proxtest(dyn,ovcheck,tol);
-                    [ermu,muix] = mu_search(mu,ovcheck,tol,'short',1);
+                    %[ermu,muix] = mu_search(mu,ovcheck,tol,'short',1);
                 end
                 
                 if il==retry, endloop=1; end
@@ -80,7 +80,7 @@ for i=1:iters
     if kill==0
         fn = fieldnames(fil); pos = []; l=0; ct = ct+1;
         dyn = [dyn;fp]; %#ok<AGROW> %dynamic overlap testing points
-        mu = mu_build(fp,muix,mu,'leafmax',leaf);
+        %mu = mu_build(fp,muix,mu,'leafmax',leaf);
         for fsl=1:numel(fn)
             pts.(fn{fsl}) = [pts.(fn{fsl});fil.(fn{fsl})]; %write fil to pts structure
         end
