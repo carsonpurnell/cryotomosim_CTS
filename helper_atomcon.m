752b2b9c93e385f841fd90a76da607f8a4668abd
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-10-18 11:08:50 -0400

    Create helper_atomcon.m

---
+++ helper_atomcon.m
@@ -0,0 +1,34 @@
+function con = helper_atomcon(box,pix,n,sc)
+if nargin<3
+    n = 4+pix^1.5;
+    sc = 2400;
+end
+sz = [max(box),max(box)]; 
+dl = 10;
+w = 1;
+ptsb = internal_gen_atomborder(sz,n/2,sc*1,4)-[0,0,dl*randi(6)*w];
+ptst = internal_gen_atomborder(sz,n/2,sc*1,4)+[0,0,dl*randi(6)*w+box(3)];
+pts = zeros(0,3);
+for i=1:4
+    pts = [pts;ptsb-[0,0,dl*i]]; pts = [pts;ptst+[0,0,dl*i]];
+end
+con = pts;
+end
+
+function pts = internal_gen_atomborder(sz,n,sc,sep)
+%n = 2.5; % noise magnitude
+%sc = 500; % scale of Z noise
+%sep = 3;
+pad = 20; %padding - scale by input size maybe? prune afterward?
+sd = max(sz)+pad*2;
+[X,Y] = ndgrid(1:sep:sd,1:sep:sd);
+i = min(X-1,sd-X+1); j = min(Y-1,sd-Y+1);
+H = exp(-.5*(i.^2+j.^2)/n^2);
+Z = real(ifft2(H.*fft2(randn(size(X))))); % 0-centered, approximately normal
+
+pts = [X(:),Y(:),Z(:)*sc];
+n = size(pts,1); ix = randperm(n); ix = ix(1:round(n/10));
+pts = pts(ix,:);
+pts(:,1:2) = pts(:,1:2)-pad;
+%size(pts)
+end
