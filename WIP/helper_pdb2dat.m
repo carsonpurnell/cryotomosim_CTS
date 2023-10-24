a59791d6cb02a59d9cb10c5689c2783f520d44b1
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-10-24 09:58:52 -0400

    Update helper_pdb2dat.m

--- WIP/helper_pdb2dat.m
+++ WIP/helper_pdb2dat.m
@@ -130,7 +130,7 @@ function piter = boundaryiter(pts)
 pts = pts(uix,:); %strip pts down
 n = size(pts,1);
 ix = randperm(n); ix = repmat(ix,[2,1]);
-iters = 5;
+iters = 2;
 l = round(n/iters);
 alpha = 12;
 
