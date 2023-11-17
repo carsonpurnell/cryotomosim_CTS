6d389cff5201c74cc73f8e48556e937964a9fb19
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-11-16 22:06:55 -0500

    Update randv.m

--- randv.m
+++ randv.m
@@ -17,7 +17,8 @@ else
     if numel(ang)==1, ang(2)=ang(1); ang(1)=0; end %if only 1 ang, use as max against min 0
     %nrep = row/size(ax,1); %number of replicates needed to match matrix size for cross
     %might be able to rework ax to be a column of axes. need to be vecnormed.
-    ax = ax/norm(ax); %unitize target vector to avoid miscalculation
+    
+    ax = ax/norm(ax); %unitize target vector
     rotax = cross(repmat(ax,row/size(ax,1),1),rax); %compute orthogonal axes to rotate
     rotax = (rotax'./vecnorm(rotax'))'; %unitize orthogonal axes
     vec = zeros(row,col);
