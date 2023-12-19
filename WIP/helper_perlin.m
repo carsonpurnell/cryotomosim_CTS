d43f92645555cd166ffd1ad41b6fc8ab524862c3
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-12-19 14:17:17 -0500

    Update helper_perlin.m

--- WIP/helper_perlin.m
+++ WIP/helper_perlin.m
@@ -10,19 +10,20 @@ end
 %w = size(grid);
 i = startoct-round(log2(pix)); %adjust frequency based on pixel size
 e = i+octaves;
-octaves = i:e;
+octaves = i:1:e;
 %l = zeros(0);
 %j = i:e;%,e,e]
-pad = 10; sz = size(gridxy)+pad;
+pad = 0; sz = size(gridxy)+pad;
 layers = zeros(sz(1),sz(2),numel(octaves));
 %m = zeros(size(grid)+pad);
 for i=1:numel(octaves)
+    oc = octaves(i);
     d = randn(sz);
-    for k=1:octaves(i) %do iterative refinements and shrinking rather than 2^k expansion in one step
+    for k=1:oc %do iterative refinements and shrinking rather than 2^k expansion in one step
         d = interp2(d, 1, 'spline');
         d = d(1:sz(1), 1:sz(2));
     end
-    layers(:,:,i) = (1.32^i) *2* d(1:sz(1), 1:sz(2));
+    layers(:,:,i) = (1.32^oc) *2* d(1:sz(1), 1:sz(2));
     %s = s + layers(:,:,i);
 end
 %s = s(pad+1:pad+size(grid,1),pad+1:pad+size(grid,2));
