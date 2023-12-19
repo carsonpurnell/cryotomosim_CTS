1638a26a13e89caec755142548c453f2593d872b
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-12-19 11:00:41 -0500

    Update helper_perlin.m

--- WIP/helper_perlin.m
+++ WIP/helper_perlin.m
@@ -1,9 +1,16 @@
-function [field,layers] = helper_perlin(gridxy,pix)
+function [field,layers] = helper_perlin(gridxy,pix,octaves,startoct)
+
+arguments
+    gridxy
+    pix
+    octaves = 10
+    startoct = 8
+end
 %sz = size(grid);
 %s = 0;%zeros(size(m));     % Prepare output image (size: m x m)
-octaves = 12;
+%octaves = 12;
 %w = size(grid);
-i = 8-round(log2(pix));
+i = startoct-round(log2(pix));
 e = i+octaves;
 octaves = i:e;
 %l = zeros(0);
@@ -20,7 +27,7 @@ for i=1:numel(octaves)%while 2 > 1 && i<e
         d = d(1:sz(1), 1:sz(2));
     end
     %d = rescale(d,-1,1);
-    layers(:,:,i) = (1.35^i) *2* d(1:sz(1), 1:sz(2));
+    layers(:,:,i) = (1.32^i) *2* d(1:sz(1), 1:sz(2));
     %s = s + layers(:,:,i);
 end
 %s = s(pad+1:pad+size(grid,1),pad+1:pad+size(grid,2));
