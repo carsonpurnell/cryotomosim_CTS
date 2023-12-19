8151a99f2bf7efda426a1db45fdbcafd89bf5175
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-12-19 10:11:25 -0500

    Update helper_perlin.m

--- WIP/helper_perlin.m
+++ WIP/helper_perlin.m
@@ -1,32 +1,31 @@
-function [field,layers] = helper_perlin(grid,pix)
+function [field,layers] = helper_perlin(gridxy,pix)
 %sz = size(grid);
-s = 0;%zeros(size(m));     % Prepare output image (size: m x m)
-w = size(grid);
+%s = 0;%zeros(size(m));     % Prepare output image (size: m x m)
+octaves = 12;
+%w = size(grid);
 i = 8-round(log2(pix));
-e = i+12;
-l = zeros(0);
-j = i:e;%,e,e]
-layers = zeros(size(grid,1),size(grid,2),numel(j));
-pad = 10;
-m = zeros(size(m)+pad);
-%i = 4;
+e = i+octaves;
+octaves = i:e;
+%l = zeros(0);
+%j = i:e;%,e,e]
+pad = 10; sz = size(gridxy)+pad;
+layers = zeros(sz(1),sz(2),numel(octaves));
+%m = zeros(size(grid)+pad);
 %change loop to a resolution-based metric - wavelength or frequency?
-for i=1:numel(j)%while 2 > 1 && i<e
-    %i = i + 2;%1;
-    jj = j(i);
-    %i=j(jj);
-    d = randn(size(m));
-    for k=1:jj %do iterative refinements and shrinking rather than 2^k expansion in one step
+for i=1:numel(octaves)%while 2 > 1 && i<e
+    %jj = j(i);
+    d = randn(sz);
+    for k=1:octaves(i) %do iterative refinements and shrinking rather than 2^k expansion in one step
         d = interp2(d, 1, 'spline');
-        d = d(1:size(m,1), 1:size(m,2));
+        d = d(1:sz(1), 1:sz(2));
     end
     %d = rescale(d,-1,1);
-    layers(:,:,i) = (1.35^i) *2* d(1:size(m,1), 1:size(m,2));
-    s = s + layers(:,:,i);
-    %w = w - ceil(w/2 - 1);
+    layers(:,:,i) = (1.35^i) *2* d(1:sz(1), 1:sz(2));
+    %s = s + layers(:,:,i);
 end
-s = s(pad+1:pad+w(1),pad+1:pad+w(2));
-l = l(:,:,2:end);
+%s = s(pad+1:pad+size(grid,1),pad+1:pad+size(grid,2));
+%l = l(:,:,2:end);
 %s = (s - min(min(s(:,:)))) ./ (max(max(s(:,:))) - min(min(s(:,:))));
+layers = layers(pad+1:pad+size(gridxy,1),pad+1:pad+size(gridxy,2),:);
 field = sum(layers,3);
 end
