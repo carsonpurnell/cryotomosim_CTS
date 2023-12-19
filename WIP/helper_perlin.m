ddcdf320dcebd1442984c1737336398f6eb04ce2
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2023-12-19 11:01:27 -0500

    Update helper_perlin.m

--- WIP/helper_perlin.m
+++ WIP/helper_perlin.m
@@ -7,10 +7,8 @@ arguments
     startoct = 8
 end
 %sz = size(grid);
-%s = 0;%zeros(size(m));     % Prepare output image (size: m x m)
-%octaves = 12;
 %w = size(grid);
-i = startoct-round(log2(pix));
+i = startoct-round(log2(pix)); %adjust frequency based on pixel size
 e = i+octaves;
 octaves = i:e;
 %l = zeros(0);
@@ -18,15 +16,12 @@ octaves = i:e;
 pad = 10; sz = size(gridxy)+pad;
 layers = zeros(sz(1),sz(2),numel(octaves));
 %m = zeros(size(grid)+pad);
-%change loop to a resolution-based metric - wavelength or frequency?
-for i=1:numel(octaves)%while 2 > 1 && i<e
-    %jj = j(i);
+for i=1:numel(octaves)
     d = randn(sz);
     for k=1:octaves(i) %do iterative refinements and shrinking rather than 2^k expansion in one step
         d = interp2(d, 1, 'spline');
         d = d(1:sz(1), 1:sz(2));
     end
-    %d = rescale(d,-1,1);
     layers(:,:,i) = (1.32^i) *2* d(1:sz(1), 1:sz(2));
     %s = s + layers(:,:,i);
 end
