5ddd4ad77c13a275267d2e0c2ee3d9bb38188e78
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-01-07 22:52:09 -0500

    Update helper_thickfromsurf.m

--- WIP/helper_thickfromsurf.m
+++ WIP/helper_thickfromsurf.m
@@ -49,3 +49,23 @@ for i=1:2
     p = p(ix,:); v = v(ix); %drop points outside the box
 end
 end
+
+function t = rotmat(ax,rad)
+ax = ax/norm(ax);
+x = ax(1); y = ax(2); z = ax(3);
+c = cos(rad); s = sin(rad);
+
+t1 = c + x^2*(1-c);
+t2 = x*y*(1-c) - z*s;
+t3 = x*z*(1-c) + y*s;
+t4 = y*x*(1-c) + z*s;
+t5 = c + y^2*(1-c);
+t6 = y*z*(1-c)-x*s;
+t7 = z*x*(1-c)-y*s;
+t8 = z*y*(1-c)+x*s;
+t9 = c+z^2*(1-c);
+
+t = [t1 t2 t3
+    t4 t5 t6
+    t7 t8 t9];
+end
