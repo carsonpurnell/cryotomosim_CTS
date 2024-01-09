8bb3be976eaf1b963c2bb034768e47f40ce167b3
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-01-09 13:51:45 -0500

    Update helper_surf.m

--- WIP/helper_surf.m
+++ WIP/helper_surf.m
@@ -3,13 +3,14 @@ function surfaces = helper_surf(box,pix,angles,axis,sc)
 %box = [400,300,40]*pix;
 %angles = -60:5:60;
 mang = max(abs(angles));
-mtilt = tand(mang)*0.6; % angle for initial grid padding to cover tilt area - don't know why 0.6 is enough
+axspec = 1+rem(axis,2);
+mtilt = tand(mang)*1.1; % angle for initial grid padding to cover tilt area - don't know why 0.6 is enough
 % make surfaces - displaced in Z
 % trying with identical surf pairs first
 res = pix/2; %oversample to prevent holes in data
 %need to stretch coverage a lot, fill values are static and not NN or average of near edge
 pad = [pix*2,pix*2]; %axis = 1; % huge padding only against tilt axis, otherwise small
-pad(axis) = round(box(axis)*mtilt); % huge padding to cover for tilting
+pad(axis) = round((box(axspec)+box(3))*mtilt)+pix*2; % huge padding to cover for tilting
 [x,y] = meshgrid(pix/2-pad(1):res:box(1)+pad(1),pix/2-pad(2):res:box(2)+pad(2));
 
 %sc = [2.5,1.2];
