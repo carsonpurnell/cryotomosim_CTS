f9e610bd7f83999fd9be41b321dceb1a1bb3da44
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-01-18 09:39:33 -0500

    Update test_tiltprojecting.m

--- WIP/test_tiltprojecting.m
+++ WIP/test_tiltprojecting.m
@@ -17,7 +17,7 @@ proj = sum(rot,3);
 
 %% functionalized tilt projection
 angles = -60:5:60;
-ax = [1,0.1,0];
+ax = [1,-0.1,0];
 
 [tilts,rot] = tiltproj(inv,angles,ax);
 %sliceViewer(rot)
@@ -72,7 +72,7 @@ for i=1:numel(angles)
     
     imrefst = imref3d(outsz,xworld,yworld,rout.ZWorldLimits);
     
-    rot = imwarp(vol,tform,'linear','OutputView',imrefst,'FillValues',0);%mean(vol,'all'));
+    rot = imwarp(vol,tform,'cubic','OutputView',imrefst,'FillValues',0);%mean(vol,'all'));
     proj = sum(rot,3); mean(proj,'all')
     tilts(:,:,i) = proj;
 end
