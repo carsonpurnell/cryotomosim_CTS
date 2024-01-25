b1420f8f4445330532447908a1b7c3ade84db27f
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-01-25 09:58:41 -0500

    Update atomicmodeling_scripttesting.m

--- WIP/atomicmodeling_scripttesting.m
+++ WIP/atomicmodeling_scripttesting.m
@@ -58,7 +58,9 @@ tic; [split,dyn,mu] = helper_randfill_atom(layers,boxsize,n,splitin,dx,dyn); toc
 
 %% function for vol, atlas, and split generation + water solvation
 outpix = pix;
+profile on
 [vol,solv,atlas,splitvol] = helper_atoms2vol(outpix,split,boxsize);
+profile viewer
 sliceViewer(vol*1+solv);
 %{
 WriteMRC(vol+solv,outpix,'solv_n1.mrc');
