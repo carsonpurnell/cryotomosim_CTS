ad93b6138c128c4e3b8a7c4c5afa637a8a5b6bce
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-02-21 12:37:16 -0500

    Update atomic_sim_test1.m

--- WIP/atomic_sim_test1.m
+++ WIP/atomic_sim_test1.m
@@ -15,7 +15,7 @@ atoms(:,1:3) = (atoms(:,1:3)-cen)*rotmat(ax,deg2rad(angle))+cen;
 
 % project a set of slices - higher resolution in Z? start with isotropy
 pix = 8;
-slabthick = 4;
+slabthick = 2;
 atoms(:,3) = (atoms(:,3)-min(atoms(:,3),[],'all'))/slabthick;
 sz = boxsize; sz(3) = max(atoms(:,3),[],'all');
 vol = helper_atoms2vol(pix,atoms,sz);
@@ -31,7 +31,7 @@ dvol = poissrnd(rescale(vol*-1)*d,size(vol));
 % propogate transmission
 mid = round(size(dvol,3)/2);
 for i=1:size(dvol,3)
-    adj = (pix*slabthick*(i-mid))/1000;
+    adj = (pix*slabthick*(i-mid))/1000*5;
     param.defocus = -5+adj;
     param.tilt = 0;
     [convolved(:,:,i), ctf, param] = helper_ctf(dvol(:,:,i),param);
