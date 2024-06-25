2f1d283383864dcf946ac560c99d9f102a031eaa
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-06-25 13:58:04 -0400

    Update atomicmodeling_scripttesting.m

--- WIP/atomicmodeling_scripttesting.m
+++ WIP/atomicmodeling_scripttesting.m
@@ -453,12 +453,16 @@ dyn{1} = [dyn{1};pad];
 kdcell = []; shapecell = [];
 mu = mu_build(dyn{1},'leafmax',1e3,'maxdepth',2);
 
+%memparam = {zeros(3,2)};
+memparam = {[300,500;.4,.6;24,6]};
+
 tol = 2; %tolerance for overlap testing
 retry = 5; %retry attempts per iteration
 count.s = 0; count.f = 0;
 lipid{1} = zeros(0,4); lipid{2} = 1;
 for i=1:memnum % simplified loop to add vesicles
-    [tpts,tperim] = gen_mem(300+randi(500),[],rand*0.6+0.4, 24+randi(8));%.3/.7 and 24/8
+    %[tpts,tperim] = gen_mem(300+randi(500),[],rand*0.6+0.4, 24+randi(6));%.3/.7 and 24/8
+    [tpts,tperim] = gen_mem(memparam{1});
     
     [err,loc,tform,ovcheck,muix] = anyloc(boxsize,tperim,dyn,retry,tol,mu);
     %{
