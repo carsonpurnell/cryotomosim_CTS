1f94f822ccf32d0d005956c1b36e8a492ef7c388
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-08-12 15:23:50 -0400

    Update cts_model_atomic.m

--- WIP/cts_model_atomic.m
+++ WIP/cts_model_atomic.m
@@ -1,4 +1,8 @@
 function [vol,solv,atlas,splitvol,acount,split,dat] = cts_model_atomic(box,input,param,opt)
+% [vol,solv,atlas,splitvol,acount,split,dat] = cts_model_atomic(box,input,param,opt)
+% ex
+% [vol,solv,atlas] = cts_model_atomic([300,400,50],'gui',{8,'mem',1,'iters',400});
+%
 
 arguments
     box
