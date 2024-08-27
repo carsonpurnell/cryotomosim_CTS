57513fafa40c469b939a85e2876bc6a729de4748
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-08-27 09:39:41 -0400

    Update trainscript_growthconesegment.m

--- scripts/trainscript_growthconesegment.m
+++ scripts/trainscript_growthconesegment.m
@@ -20,9 +20,9 @@ ptable.pix(1:n) = 12+round(rand(n,1)*20)/10; %12-14 angstrom size/scale variatio
 ptable.thick(1:n) = 45+round(10*rand(n,1)); % 45-55 pixel thickness for SNR/orientation variation
 ptable.iters(1:n) = 200+10*round(40*rand(n,1)); % 200-600 (increment 10) iterations
 %500+5*(round(50*(rand(n,1)-rand(n,1)))); %500+-250 (50 increments) iterations
-ptable.mem(1:n) = randi(6,n,1); %1-6 membranes
+ptable.mem(1:n) = randi(10,n,1); %1-6 membranes
 
-% simulating params
+% simulation params
 ptable.dose(1:n) = 80+round(80*rand(n,1)); %80-120 dose, uniform distribution
 ptable.defocus(1:n) = -4-round(10*rand(n,1))/5; % -4 to -6 defocus
 % radiation, tilting?
@@ -31,12 +31,11 @@ ptable.defocus(1:n) = -4-round(10*rand(n,1))/5; % -4 to -6 defocus
 % small range of xy sizes, mostly to have differently oriented rectangles? or extraneous? complex reporting
 % same density cap for all layers for simplicity
 % 2 target layers of fixed components + distractor layer of randomized grit
-% run models
 
 digits = numel(num2str(n));
 fspec = append('%0',num2str(digits),'i');
 for i=1:n
-    vol = zeros([300,300,ptable.thick(i)]);
+    vol = zeros([400,400,ptable.thick(i)]);
     linput = targets;
     %tl = helper_input(linput,ptable.pix(i));
     modelparam = param_model(ptable.pix(i),'layers',linput,'iters',ptable.iters(i),'mem',ptable.mem(i));
@@ -45,7 +44,8 @@ for i=1:n
     [cts,outfile] = cts_model(vol,modelparam,'suffix',suf);
     
     %atomic or volumetric model? atomic membrane proteins still not ready
-    [~,~,~,atlas] = cts_simulate(outfile,{'dose',ptable.dose(i),'defocus',ptable.defocus(i)},'suffix','bulksim');
+    simparam = param_simulate('dose',ptable.dose(i),'defocus',ptable.defocus(i));
+    [~,~,~,atlas] = cts_simulate(outfile,simparam,'suffix','bulksim');
     ptable.classes(i) = numel(unique(atlas)); % class count to ensure full coverage
 end
 disp(ptable) % display table of parameters used
