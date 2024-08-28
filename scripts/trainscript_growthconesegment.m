68d74f77be9b37efe8fc47552ff0d5cbda7f5ec1
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-08-28 11:51:26 -0400

    Update trainscript_growthconesegment.m

--- scripts/trainscript_growthconesegment.m
+++ scripts/trainscript_growthconesegment.m
@@ -5,11 +5,12 @@
 targets = {'ribo__ribo__4ug0_4v6x.group.mat',... % two ribo variations
     'cofilactin__cofilactin__actin__actin__x3-x4_long.bundle.pdb'... % two lengths each
     'MT__6o2tx3.mat',... % long MT stretch
-    'tric__tric__6nra-open_7lum-closed.group.pdb'}; % two tric variations
+    'tric__tric__6nra-open_7lum-closed.group.mat'}; % two tric variations
 %anything else?
+
 % need all members, randomly add 1-2 replicates to shift occupancies?
 
-%distractors = {}
+distractors = {'act1-A2.distract.mat','1tub_Distractor.mat'};
 % pool of distractors to pull random sample from to use as second/third? layer in modeling
 %act/tub monomers, tiny mem decorations, 
 
@@ -18,14 +19,13 @@ ptable = table; % initialize table of parameters, one row per run
 
 % modeling params
 ptable.pix(1:n) = 12+round(rand(n,1)*20)/10; %12-14 angstrom size/scale variation
-ptable.thick(1:n) = 45+round(10*rand(n,1)); % 45-55 pixel thickness for SNR/orientation variation
-ptable.iters(1:n) = 200+10*round(60*rand(n,1)); % 200-800 (increment 10) iterations
-%500+5*(round(50*(rand(n,1)-rand(n,1)))); %500+-250 (50 increments) iterations
-ptable.mem(1:n) = randi(15,n,1); %1-15 membranes
+ptable.thick(1:n) = 40+round(20*rand(n,1)); % 45-55 pixel thickness for SNR/orientation variation
+ptable.iters(1:n) = 100+10*round(50*rand(n,1)); % 100-600 (increment 10) iterations
+ptable.mem(1:n) = randi(12,n,1); %1-12 membranes
 %randomized beads?
 
 % simulation params
-ptable.dose(1:n) = 80+round(80*rand(n,1)); %80-120 dose, uniform distribution
+ptable.dose(1:n) = 70+round(80*rand(n,1)); %70-150 dose, uniform distribution
 ptable.defocus(1:n) = -4-round(10*rand(n,1))/5; % -4 to -6 defocus
 % radiation, tilting?
 
@@ -35,13 +35,20 @@ ptable.defocus(1:n) = -4-round(10*rand(n,1))/5; % -4 to -6 defocus
 % 2 target layers of fixed components + distractor layer of randomized grit
 
 digits = numel(num2str(n));
-fspec = append('%0',num2str(digits),'i');
+fspec = append('%0',num2str(digits),'i'); %formatspec for suffixes
 for i=1:n
     vol = zeros([400,400,ptable.thick(i)]);
-    linput = targets;
+    distix = logical(randi(2,1,numel(distractors))-1);
+    if all(distix==false), distix(randi(numel(distix)))=1; end %ensure at least one distractor
+    ptable.distractors(i,1:numel(distix)) = single(distix);
+    linput = {targets,distractors(distix)};
     %tl = helper_input(linput,ptable.pix(i));
-    modelparam = param_model(ptable.pix(i),'layers',linput,'iters',ptable.iters(i),...
+    dparam = param_model(ptable.pix(i),'layers',distractors(distix));
+    modelparam = param_model(ptable.pix(i),'layers',targets,'iters',ptable.iters(i),...
         'mem',ptable.mem(i),'beads',10);
+    modelparam.layers{2} = dparam.layers{1}; %add distractors into layer
+    modelparam.iters(2) = modelparam.iters(1); modelparam.density(2) = modelparam.density(1);
+    % distractor iters too?
     
     suf = append('batch_',num2str(i,fspec));
     [cts,outfile] = cts_model(vol,modelparam,'suffix',suf);
