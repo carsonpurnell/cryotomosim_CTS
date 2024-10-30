78595768eb222e9f4c6d23eb4d157977db1ef200
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2024-10-30 16:19:53 -0400

    Update randv.m

--- randv.m
+++ randv.m
@@ -1,16 +1,27 @@
-function vec = randv(row,col,ax,ang) %ang IN RADIANS
-% vec = randv(row,col)
-% generates an array the size of row*col, with one vector per row on a uniform unit sphere
-% advanced usage for nx3 vectors:  vec = randv(row,col,ax,ang)
-% these vectors are incread constrained about the vector ax by the limits ang
+function vec = randv(row,col,ax,ang) 
+% vec = randv(row,col,ax,ang) % ang is an angle in RADIANS
+% limitation: only working for 1x3 vectors (relies on exclusively 1x3 cross function)
+% generates row vectors of length col on a unit sphere, under constraints in ax/ang (optional)
+% if ax/ang are input, vectors are constrained along those axes up to ang offset angle
+% vectors are NOT uniformly spaced, but uniformly deflected from the axis (polar density bias)
+% ang can be 1x2 for both minimum and maximum deflection (order invariant)
+% if ang is input but ax is null, a random axis is used
+% ax can accept a list of axes, replicating them to row*col size for ouput (may change, it was a weird choice)
+%
 % ax must be a 1x3 vector, or empty. if empty, a random axis is used for the entire set
 % ang are the constraints in radians. either [min,max] or [max] that defaults to min=0
-% this is the deflection angle from ax. [0,pi] for hemisphere, [80,100] for a narrow belt, etc
-% the per-vector angle is generated with a uniform distribution, producing a density bias near ax poles
+% this is the deflection angle from ax. [0,pi] for hemisphere, [0.4,0.5] for a narrow belt, etc
+
+arguments
+    row
+    col
+    ax = []
+    ang = []
+end
 
 %rax = randvec(row,col); %random vectors - finished or to cross with the center axis
 rax = randn(col,row); rax = (rax./vecnorm(rax))'; %random initial vectors
-if (isempty(ax) && isempty(ang)) || col~=3
+if (isempty(ax) && isempty(ang)) %|| col~=3
     vec=rax; % if ax and ang are empty, return the random unconstrained vectors
 else
     if isempty(ax), ax = randn(col,1); ax = (ax./vecnorm(ax))'; end %if no axis given, randomize one
@@ -19,11 +30,11 @@ else
     %might be able to rework ax to be a column of axes. need to be vecnormed.
     
     % now distributes outputs equally among a list of input axes
-    ax = (ax'./vecnorm(ax'))'; %unitize target vector
-    nrep = ceil(row/size(ax,1)); %replicates of axes to outputs
-    axrep = repmat(ax,nrep,1); axrep = axrep(1:row,1:col);
-    rotax = cross(axrep,rax,2); %compute orthogonal axes to rotate
-    rotax = (rotax'./vecnorm(rotax'))'; %unitize orthogonal axes
+    ax = (ax'./vecnorm(ax'))'; % unitize target vector
+    nrep = ceil(row/size(ax,1)); % needed number of axes for outputs
+    axrep = repmat(ax,nrep,1); axrep = axrep(1:row,1:col); % replicate input axes to needed length
+    rotax = cross(axrep,rax,2); % compute orthogonal axes to rotate
+    rotax = (rotax'./vecnorm(rotax'))'; % unitize orthogonal axes
     vec = zeros(row,col);
     
     for i=1:row %loop because matrix multiplication can't be vectorized?
