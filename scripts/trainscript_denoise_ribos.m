b50c0deb653706cf964b06a45d1fa00935987817
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2025-10-16 14:40:38 -0400

    Update trainscript_denoise_ribos.m

--- scripts/trainscript_denoise_ribos.m
+++ scripts/trainscript_denoise_ribos.m
@@ -1,10 +1,11 @@
 
 fname = 'ribo_4a';
 pmod = param_model(4,'iters',200,'mem',0,'grid',0);
-[cts,~,~,~,~,~,~,~,~,outfile] = cts_model_atomic([800,800,120],1,pmod,'suffix',append(fname,'_8'),'dynamotable',1);
+[cts,~,~,~,~,~,~,~,~,outfile] = cts_model_atomic([800,800,120],1,pmod,'suffix',append(fname,'_9'),'dynamotable',1);
 [path,name,ext] = fileparts(outfile);
 outfile = fullfile(path,append(name,'.atom.mat')); %bake into sim function?
 
 %%
-psim = param_simulate('pix',4,'defocus',-3,'dose',300,'raddamage',0.01,'tilt',[-40,1,40]);
-cts_simulate_atomic(outfile,psim,'suffix','test_ideal_40-1');
+a = 80;
+psim = param_simulate('pix',4,'defocus',-3,'dose',a*5,'raddamage',0,'tilt',[-a,1,a]);
+cts_simulate_atomic(outfile,psim,'suffix','test_ideal_80-1');
