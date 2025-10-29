f19ad912418e1e3fb08eb457b2dd7cb0a0f4d027
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2025-10-29 12:41:39 -0400

    Update trainscript_denoise_ribos.m

--- scripts/trainscript_denoise_ribos.m
+++ scripts/trainscript_denoise_ribos.m
@@ -5,11 +5,11 @@ pix = 4;
 pmod = param_model(pix,'layers',targs,'iters',200,'mem',0,'grid',0);
 psim1 = param_simulate('pix',pix,'defocus',-4,'dose',60,'raddamage',2,'tilterr',1,'tilt',[-60,2,60]);
 psim2 = param_simulate('pix',pix,'defocus',-4,'dose',120,'raddamage',0.5,'tilterr',0.5,'tilt',[-60,2,60]);
-psim3 = param_simulate('pix',pix,'defocus',-3,'dose',300,'raddamage',0.2,'scatter',0,'tilterr',0,'tilt',[-60,2,60]);
+psim3 = param_simulate('pix',pix,'defocus',-3,'dose',300,'raddamage',0.1,'scatter',0,'tilterr',0,'tilt',[-60,2,60]);
 pideal = param_simulate('pix',pix,'defocus',-2,'dose',500,'raddamage',0,'scatter',0,'tilt',[-80,2,80]);
 % not enough difference - bad ice implementation during tilts eroding signal?
 
-for i=1:1
+for i=1:4
 [cts,~,~,~,~,~,~,~,~,outfile] = cts_model_atomic([800,800,120],1,pmod,'suffix',append(fname,'_',string(i)),'dynamotable',1);
 [path,name,ext] = fileparts(outfile);
 outfile = fullfile(path,append(name,'.atom.mat')); %bake into sim function?
