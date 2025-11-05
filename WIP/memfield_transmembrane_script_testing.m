44c00aa0511f7cb3ac9abb778bfa8a4eef9e14fd
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2025-11-05 12:44:11 -0500

    Update memfield_transmembrane_script_testing.m

--- WIP/memfield_transmembrane_script_testing.m
+++ WIP/memfield_transmembrane_script_testing.m
@@ -1,5 +1,23 @@
 %% placing memprots with new atomic membrane structure
+pix = 10;
+targ = {'ATPS.membrane.complex.mat'};
+pmod = param_model(pix,'layers',targ);
 
-memdat = gen_mem_atom([200,200,50],10);
+sz = [200,200,50];
+memdat = gen_mem_atom(sz,pix);
+% needs a bit more work, a few vectors (probably due to corners) are not well-oriented
+
+%%
+for i=1:numel(pmod.layers)
+    for j=1:numel(pmod.layers{i}.id)
+        atoms.(pmod.layers{i}.id{j}) = zeros(0,4);
+    end
+end
+
+%pick a membrane
+memsel = randi(numel(memdat));
 
 
+% start off preselecting coords from it or just start running through them? they are not spatially ordered
+% inner loop: random axial rotation, rotation to transmembrane vector, collision test
+% if no collision, switch to place subunits as needed
