11839fa5cbb59293a5474b2e19396497ac0b2804
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2025-11-05 14:25:01 -0500

    Update memfield_transmembrane_script_testing.m

--- WIP/memfield_transmembrane_script_testing.m
+++ WIP/memfield_transmembrane_script_testing.m
@@ -1,50 +1,94 @@
 %% placing memprots with new atomic membrane structure
-pix = 10;
+pix = 12;
 targ = {'ATPS.membrane.complex.mat'};
+targ = {'GABAar.membrane.complex.mat'};
 pmod = param_model(pix,'layers',targ);
 
-sz = [200,200,50];
+sz = [300,300,60];
 memdat = gen_mem_atom(sz,pix);
-% needs a bit more work, a few vectors (probably due to corners) are not well-oriented
+% needs a bit more work, a few vectors (probably due to corners) are not well-oriented - denser mesh?
 
 %%
+split = struct; dx = struct; list = struct;
+layers = pmod.layers;
+for i=1:numel(layers)
+namelist = [layers{i}.modelname]; %slower than cell, but more consistent
+for j=1:numel(namelist)
+    if ~isfield(split,namelist{j})
+        split.(namelist{j}) = zeros(0,4); %initialize split models of target ids
+        list.(namelist{j}) = zeros(0,3);
+    end
+    if ~isfield(dx,namelist{j})
+        dx.(namelist{j}) = size(split.(namelist{j}),1)+1;
+    end
+end
+end
+%memat = vertcat(memdat.memcell{:});
+%memat(:,4) = 3;
+%split.mem = memdat.atoms.vesicle;
+
 for i=1:numel(pmod.layers)
     for j=1:numel(pmod.layers{i}.id)
         atoms.(pmod.layers{i}.id{j}) = zeros(0,4);
     end
 end
-dyn{1} = single(zeros(0,3)); dyn{2} = 0;
+dyn{1} = single(zeros(0,3)); dyn{2} = 1;
 leaf = 1e3;
 mu = mu_build(dyn{1},[0,0,0;sz*pix],'leafmax',leaf,'maxdepth',2);
 
 %pick a membrane
 memsel = randi(numel(memdat));
+init = [0,0,-1];
 
 sel = pmod.layers{1}(1);
 if any(ismember(sel.flags,'complex'))
     sel.sumperim = vertcat(sel.perim{:});
     complex = 1;
 else
+    sel.sumperim = sel.perim{1};
     complex = 0;
 end
 % start off preselecting coords from it or just start running through them? they are not spatially ordered
-iters = 15;
+iters = 50;
 for i=1:iters
     % inner loop: random axial rotation, rotation to transmembrane vector, collision test
-    r1 = rand*2*pi;
-    rot1 = sel.sumperim*rotmat([0,0,1],r1); % random axial rotation
+    % mem vector and figuring stuff
+    memloc = memdat.memcell{memsel}(i,:); % selected coordinate
+    surfvec = memdat.normcell{memsel}(i,:); % normal vector to surface at coordinate
+    
+    rotax=cross(init,surfvec); %compute the normal axis from the rotation angle
+    theta = acos( dot(init,surfvec) ); % angle between ori vec and surfae
+    
+    spinang = rand*2*pi;
+    rot1 = sel.sumperim*rotmat(init,spinang); % random axial rotation
+    rot2 = rot1*rotmat(rotax,theta)+memloc;
     
     
+    
+    err = 0; % force placement, no collision test yet
     
     % if no collision, switch to place subunits as needed after replicating rotations
     if err==0
-        tpts = sel.adat{sub};
-        tpts(:,1:3) = transformPointsForward(tform,tpts(:,1:3))+loc;
+        [dyn] = dyncell(rot2,dyn); % write to partial list for fast lookups (obsolete under mu?)
+        % no muix yet, not testing
+        % mu = mu_build(rot2,muix,mu,'leafmax',leaf,'maxdepth',2);
         
-        [dyn] = dyncell(ovcheck,dyn);
-        mu = mu_build(ovcheck,muix,mu,'leafmax',leaf,'maxdepth',2);
-        [split,dx] = dynsplit(tpts,split,dx,sel.modelname{sub});
-        list.(sel.modelname{sub})(end+1,:) = loc;
+        if complex ==1
+            for u=1:numel(sel.id)
+                tmp = sel.adat{u};
+                tmp(:,1:3) = tmp(:,1:3)*rotmat(init,spinang);
+                tmp(:,1:3) = tmp(:,1:3)*rotmat(rotax,theta)+memloc;
+                
+                [split,dx] = dynsplit(tmp,split,dx,sel.id{u});
+                list.(sel.id{u})(end+1,:) = memloc;
+            end
+        else
+            
+        end
+        %tpts = sel.adat{sub};
+        %tpts(:,1:3) = transformPointsForward(tform,tpts(:,1:3))+loc;
+        
+        
         %{
         %[dynfn,dynfnix] = fcndyn(ovcheck,dynfn,dynfnix); % insignificantly slower than inlined
         %[dyn{1},dyn{2}] = dyncat(dyn{1},dyn{2},ovcheck); %6s
@@ -76,9 +120,12 @@ for i=1:iters
         %count.s=count.s+1;
     else
         %count.f=count.f+1;
+        disp('fail, something borked')
     end
 end
-
+[vol,solv] = helper_atoms2vol(pix,split,sz*pix);
+mvol = helper_atoms2vol(pix,memdat.atoms,sz*pix);
+sliceViewer(max(vol,mvol));
 
 
 %% internal functions
@@ -95,6 +142,24 @@ for r=1:retry
     if err==0, break; end
 end
 end
+
+function [dyn] = dyncell(addpts,dyn)
+    l = size(addpts,1); e = dyn{2}+l-1;
+    if e>size(dyn{1},1)
+        dyn{1}(dyn{2}:(size(dyn{1},1)+l)*3,:) = 0;
+    end
+    dyn{1}(dyn{2}:e,:) = addpts;
+    dyn{2} = dyn{2}+l;
+end
+function [split,dx] = dynsplit(tpts,split,dx,splitname) %slower than inlined a bit
+tdx = dx.(splitname);
+l = size(tpts,1); e = tdx+l-1;
+if e>size(split.(splitname),1)
+    split.(splitname)(tdx:(tdx+l)*2,:) = 0;
+end
+split.(splitname)(tdx:e,:) = tpts; dx.(splitname) = tdx+l;
+end
+
 
 function t = rotmat(ax,rad)
 ax = ax/norm(ax);
