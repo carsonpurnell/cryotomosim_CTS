9a6980cdd09cfd23a68f4c485bfe756776ee0b9e
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2025-12-01 13:07:32 -0500

    Update gen_mem_atom.m

--- WIP/gen_mem_atom.m
+++ WIP/gen_mem_atom.m
@@ -185,7 +185,7 @@ for i=1:numel(minit)
     tmp2 = randtess(0.5,sh1,'s'); % sometimes has wacky infills from incomplete internal tesselation
     
     %initshape{i} = sh1;
-    [tmp,head,tail,shell,mesh] = shape2mem(sh1,thick,pix/2);
+    [tmp,head,tail,shell,mesh] = shape2mem(sh1,thick,pix/1.5);
     % currently very wiggly, quite possibly too wiggly
     % denser mesh to reduce the wiggle? or smiter iters in already very round mems?
     
@@ -214,7 +214,7 @@ vec = randn(size(head)); % random displacement directions for head density
 spd = (rand(size(vec,1),1)-rand(size(vec,1),1))*(thick*0.2); % triangular random displacement distances
 vec = vec./vecnorm(vec,2,2).*spd; % displacement vectors
 head=head+vec; atoms = [head;tail];
-atoms(:,4) = 5.6/1 *atomfrac; % magnitude of pseudoatoms
+atoms(:,4) = 5.5/1 *atomfrac; % magnitude of pseudoatoms
 end
 
 function [cen,pts] = voronoirelax(pts,cen,iters,weight)
