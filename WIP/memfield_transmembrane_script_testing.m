8976693b19a4d6752eea6c0dbd0e4a3cdf38b58b
carsonpurnell <111076771+carsonpurnell@users.noreply.github.com>
2025-12-05 13:14:28 -0500

    Update memfield_transmembrane_script_testing.m

--- WIP/memfield_transmembrane_script_testing.m
+++ WIP/memfield_transmembrane_script_testing.m
@@ -3,7 +3,7 @@ pix = 12;
 targ = {'ATPS.membrane.complex.mat'};
 %targ = {'ATPS.membrane.mat'};
 %targ = {'GABAar.membrane.complex.mat'};
-targ = {'ATPS.membrane.complex.mat','GABAar.membrane.complex.mat','ATPS__flip.6j5i.membrane.cif'};
+targ = {'GABAar.membrane.complex.mat','ATPS__flip.6j5i.membrane.cif'}; %flip.mat not flipped?
 pmod = param_model(pix,'layers',targ);
 
 sz = [300,300,100];
